76adbc7cf8c73f1d9d486475930e8b5d0a2e1681
Leon Ho <leonhwh@icloud.com>
2018-06-07 23:40:18 +0800

    fix draw function to reset gameMoves

--- script.js
+++ script.js
@@ -12,12 +12,15 @@ function currentPlayer(x){
 }
 
 //after refactoring
+//id is passed from index.html onclick="clickId(this.id)"
 function clickId(id){
+//checks for player turns using players object and apply X or O
   if(players.currentPlayer == 'player1'){
     document.getElementById(id).innerHTML = "X";
     document.getElementById(id).removeAttribute('onclick');
     document.getElementById('winMsg').innerHTML = "Player 2's turn"
     players.currentSign = "X";
+    //stores the current value in the object for checkWin0()
     checkWinO();
     currentPlayer('player2');
     gameMoves++;
@@ -122,6 +125,8 @@ function clickId(id){
 // total of 8 wins conditions for each letter, total of 16
 
 function checkWinO(){
+  //checks for first two conditions first, when the player clicks on the last winning square, conditionals will return true and run winToast();
+  //players.currentSign is assigned from clickId();
   if(document.getElementById(boxArray[0]).innerHTML == players.currentSign && document.getElementById(boxArray[1]).innerHTML == players.currentSign && document.getElementById(boxArray[2]).innerHTML == players.currentSign){
     winToast()
   }else if (document.getElementById(boxArray[3]).innerHTML == players.currentSign && document.getElementById(boxArray[4]).innerHTML == players.currentSign && document.getElementById(boxArray[5]).innerHTML == players.currentSign) {
@@ -165,8 +170,10 @@ function checkWinO(){
 function winToast(){
   document.getElementById('winMsg').innerHTML = players.currentPlayer + " " + "Wins"
 }
+//
 function reset(){
   currentPlayer('player1');
+  gameMoves = 1;
   document.getElementById('winMsg').innerHTML = "Player 1 start!"
   var x = document.getElementsByClassName('box');
   for (var i = 0; i < x.length; i++) {
