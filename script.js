2e9cd24a8d235387d2c107896523dd3435aa17bf
jodich <jodi_choo@hotmail.com>
2018-06-08 09:29:55 +0800

    updated 8th June

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// STUCK! First round works fine, but after clicking reset btn, the player is required to click once more to show reset button
-
-
 document.addEventListener('DOMContentLoaded',function(event){
 
 allBoxes = document.querySelectorAll('span');
@@ -16,6 +13,9 @@ var c2 = document.querySelector('#c2');
 var c3 = document.querySelector('#c3');
 
 var clickCount = 0;
+if (clickCount === 0) {
+	document.querySelector('.turn').innerHTML = "It is player 1's turn"
+}
 
 // hide reset button
 hideBtn()
@@ -26,12 +26,15 @@ var displayStuff = function() {
 
 	if (clickCount % 2 === 0) {
 		this.innerHTML = 'X'
+		document.querySelector('.turn').innerHTML = "It is player 2's turn"
 	} else if (clickCount % 2 === 1) {
 		this.innerHTML = 'O'
+		document.querySelector('.turn').innerHTML = "It is player 1's turn"
 	}
 	clickCount = clickCount + 1;
 
-	this.removeEventListener('click', displayStuff);
+	this.removeEventListener('click', displayStuff);	// stops the span box from recieving any more click displayStuff events
+
 }
 
 var checkSquares = function () {
@@ -49,22 +52,63 @@ var checkSquares = function () {
 
 	// comparing the values
 	if ((a1Val ===  "X" || a1Val ===  "O") && a2Val === a1Val && a3Val === a1Val) {
+		if (a1Val === "X") {
+			console.log("Player 1 won")
+		} else {
+			console.log("Player 2 won")
+		}
 		gameOver();
 	} else if ((b1Val ===  "X" || b1Val ===  "O") && b2Val === b1Val && b3Val === b1Val) {
+		if (b1Val === "X") {
+			console.log("Player 1 won")
+		} else {
+			console.log("Player 2 won")
+		}
 		gameOver();
 	} else if ((c1Val ===  "X" || c1Val ===  "O") && c2Val === c1Val && c3Val === c1Val) {
+		if (c1Val === "X") {
+			console.log("Player 1 won")
+		} else {
+			console.log("Player 2 won")
+		}
 		gameOver();
 	} else if ((a1Val ===  "X" || a1Val ===  "O") && b1Val === a1Val && c1Val === a1Val) {
+		if (a1Val === "X") {
+			console.log("Player 1 won")
+		} else {
+			console.log("Player 2 won")
+		}
 		gameOver();
 	} else if ((a2Val ===  "X" || a2Val ===  "O") && b2Val === a2Val && c2Val === a2Val) {
+		if (a2Val === "X") {
+			console.log("Player 1 won")
+		} else {
+			console.log("Player 2 won")
+		}
 		gameOver();
 	} else if ((a3Val ===  "X" || a3Val ===  "O") && b3Val === a3Val && c3Val === a3Val) {
+		if (a3Val === "X") {
+			console.log("Player 1 won")
+		} else {
+			console.log("Player 2 won")
+		}
 		gameOver();
 	} else if ((a1Val ===  "X" || a1Val ===  "O") && b2Val === a1Val && c3Val === a1Val) {
+		if (a1Val === "X") {
+			console.log("Player 1 won")
+		} else {
+			console.log("Player 2 won")
+		}
 		gameOver();
 	} else if ((a3Val ===  "X" || a3Val ===  "O") && b2Val === a3Val && c1Val === a3Val) {
+		if (a3Val === "X") {
+			console.log("Player 1 won")
+		} else {
+			console.log("Player 2 won")
+		}
 		gameOver();
 	} else if (clickCount >= 9) {
+		console.log("Nobody won")
 		gameOver();
 	}
 
@@ -72,19 +116,18 @@ var checkSquares = function () {
 
 
 var gameOver = function() {
-	
-	console.log("game over");
 
-	document.querySelector('.btn').setAttribute("style", "display: block");
+	document.querySelector('.btn').setAttribute("style", "display: block"); // turn on reset button to reset game
+	document.querySelector('.turn').setAttribute("style", "display: none"); // turn off turn display
 	
-	document.querySelector('.btn').addEventListener('click', resetGame);
+	document.querySelector('.btn').addEventListener('click', resetGame); // when button is click, loads reset game functions
 
 	for (var i = 0; i < allBoxes.length; i++) {
-		allBoxes[i].removeEventListener('click', displayStuff)
+		allBoxes[i].removeEventListener('click', displayStuff)	// removes all eventslistener
 	}
 
 	for (var i = 0; i < allBoxes.length; i++) {
-		allBoxes[i].removeEventListener('click', checkSquares)
+		allBoxes[i].removeEventListener('click', checkSquares)	// removes all eventslistener
 	}
 
 }
@@ -99,7 +142,8 @@ var resetGame = function() {
 
 	clickCount = 0;
 
-	hideBtn ()
+	hideBtn (); // hide button
+	document.querySelector('.turn').setAttribute("style", "display: block"); // turn on turn display
 
 	for (var i = 0; i < allBoxes.length; i++) {
 		allBoxes[i].innerHTML = ""
