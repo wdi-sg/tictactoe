b6ed2e5cbca37a92b774b23fc435fcd5b833471d
martinhilary <martinhilary96@gmail.com>
2018-06-09 18:25:50 +0800

    troubles

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-// var body=document.querySelector("body");
-// var bigBox= document.createElement("div");
-// bigBox.setAttribute("id", "big");
-// body.appendChild(bigBox);
-// bigBox.style.width="300px";
-// bigBox.style.height="300px";
-// bigBox.style.border= "5px solid black";
-// bigBox.style.position="relative";
-// bigBox.style.margin="0 auto";
+var body=document.querySelector("body");
+var bigBox= document.createElement("div");
+bigBox.setAttribute("id", "big");
+body.appendChild(bigBox);
+bigBox.style.width="500px";
+bigBox.style.height="400px";
+// bigBox.style.border="5px solid black";
+// bigBox.style.display="none";
+// bigBox.style.border="none";
+bigBox.style.margin="0 auto";
+
 
 // var smallBox1= document.createElement("div")
 // smallBox1.setAttribute("class","small")
@@ -40,19 +42,19 @@
 // smallBox4.style.borderBottom="5px solid black";
 // smallBox4.style.position="absolute";
 // bigBox.appendChild(smallBox4);
-var body=document.querySelector("body");
-var bigBox= document.createElement('div');
-body.appendChild(bigBox);
-bigBox.style.width="330px";
-bigBox.style.height="300px";
-bigBox.style.border= "5px solid black";
-bigBox.style.margin="0 auto";
+// var body=document.querySelector("body");
+// var bigBox= document.createElement('div');
+// body.appendChild(bigBox);
+// bigBox.style.width="330px";
+// bigBox.style.height="300px";
+// bigBox.style.border= "5px solid black";
+// bigBox.style.margin="0 auto";
 
 
 var createBox=function(id,className){
 	var result =document.createElement('div');
-	result.style.width='100px';
-	result.style.height='90px';
+	result.style.width='150px';
+	result.style.height='140px';
 	result.style.border= "5px solid black";
 	result.style.display="inline-block";
 	result.style.marginBottom="-4px";
@@ -76,61 +78,218 @@ createBox('c2',boxclassName),
 createBox('c3',boxclassName)
 ]
 
+//Change image of each box
+var a1= document.querySelector("#a1");
+a1.style.borderRight="none";
+a1.style.borderLeft="none";
+a1.style.borderTop="none";
+
+var a2=document.querySelector("#a2")
+a2.style.borderRight="none";
+a2.style.borderTop="none";
+
+var a3=document.querySelector("#a3")
+a3.style.borderTop="none";
+a3.style.borderRight="none";
+
+var b1=document.querySelector("#b1")
+b1.style.borderRight="none";
+b1.style.borderLeft="none";
+
+
+var b2=document.querySelector("#b2")
+b2.style.borderRight="none";
+
+
+var b3=document.querySelector("#b3")
+b3.style.borderRight="none";
+
+
+var c1=document.querySelector("#c1")
+c1.style.borderRight="none";
+c1.style.borderLeft="none";
+c1.style.borderBottom="none";
+
+
+var c2=document.querySelector("#c2")
+c2.style.borderRight="none";
+c2.style.borderBottom="none";
+
+
+var c3=document.querySelector("#c3")
+c3.style.borderRight="none";
+c3.style.borderBottom="none";
+
+var clickX =function(){
+allMyBoxes.forEach(function(element){
+	element.addEventListener("click",displayX)
+})
+}
+var clickO=function(){
+	allMyBoxes.forEach(function(element){
+	element.addEventListener("click",displayO)
+})
+}
 var displayX=function(){
-	var attack= document.createElement("img");
-	attack.setAttribute("id", "attack");
-	attack.style.height="10px";
-	attack.style.width="10px";	
-	allMyBoxes[i].style.backgroundColor="red";
-	allMyBoxes[i].appendChild(attack)
-	check();
-	displayO();//oppnents move 
+	this.style.backgroundImage = "url('X.png')";
+	clickO();
+
+	// checkWin(this.id);
+	// check();
+	// displayO();//oppnents id 
 	//remove current array
 }
-for(i=0;i<allMyBoxes.length;i++){
-allMyBoxes[i].addEventListener("click",displayX);
-}//how to return i;
-// var move=document.getElementbyId(allMyBoxes[i])
-//oppponent=Math.floor(Math.random()*9-1)
-// displayO[opponenet]
-// checkWinStates
-var check=function(){
-if(move == a1 && move ==a2 && move==a3)
-{
-	alert("You won!");
+
+var displayO=function(){
+	this.style.backgroundImage="url(O.png)";
+	clickX();
+	// checkWin(this.id);
 }
-else if (move == b1 && move ==b2 && move==b3)
+
+var Xbutton= document.querySelector("#submitX")
+var Obutton= document.querySelector("#submitO")
+Xbutton.addEventListener("click", clickX);
+Obutton.addEventListener("click", clickO);
+
+
+var checkWin=function(id){
+if(id == a1 && id == b1 && id== c1)
+{
+	alert("you won")
+}
+else if (id == b1 && id ==b2 && id==b3)
 	{
 		alert("You won!");	
 	}
-else if (move == c1 && move ==c2 && move==c3)
+else if (id == c1 && id ==c2 && id==c3)
 		{
 			alert("You won!");
 		}
-else if (move == a1 && move ==b1 && move==c1)
+else if (id == a1 && id ==b1 && id==c1)
 			{
 				alert("You won!");
 			}
-else if (move == a2 && move ==b2 && move==c2)
+else if (id == a2 && id ==b2 && id==c2)
 				{
 					alert("You won!");
 				}
-else if (move == a3 && move ==b3 && move==c3)
+else if (id == a3 && id ==b3 && id==c3)
 					{
 						alert("You won!");
 					}
-else if (move == a1 && move ==b2 && move==c3)
+else if (id == a1 && id ==b2 && id==c3)
 						{
 							alert("You won!");
 						}
-else if (move == c1 && move ==b2 && move==a3)
+else if (id == c1 && id ==b2 && id==a3)
 							{
 						
 								alert("You won!");
 							}
-else{
-
-	checkEmpty();//check whether all spaces filled up and then return you lost;
+else
+{
+	alert("error");
+}
 }
 
-}
+
+//create another function to store id and then display
+
+//how to return i;
+
+// //answer in phone and also the setlink function;
+
+// allMyBoxes[i].forEach(function(allMyBox,i){
+
+// allMyBox.addEventListener("click",displayX);
+// //how to return i;
+// }
+
+// oppponent=Math.floor(Math.random()*9-1)
+// displayO[opponenet]
+// 4 ways to checkWinStates
+// var check=function(){
+// if(id == a1 && id ==a2 && id==a3)
+// {
+// 	alert("You won!");
+// }
+//Hard-coded way
+// var function= checkWin(){
+// else if (id == b1 && id ==b2 && id==b3)
+// 	{
+// 		alert("You won!");	
+// 	}
+// else if (id == c1 && id ==c2 && id==c3)
+// 		{
+// 			alert("You won!");
+// 		}
+// else if (id == a1 && id ==b1 && id==c1)
+// 			{
+// 				alert("You won!");
+// 			}
+// else if (id == a2 && id ==b2 && id==c2)
+// 				{
+// 					alert("You won!");
+// 				}
+// else if (id == a3 && id ==b3 && id==c3)
+// 					{
+// 						alert("You won!");
+// 					}
+// else if (id == a1 && id ==b2 && id==c3)
+// 						{
+// 							alert("You won!");
+// 						}
+// else if (id == c1 && id ==b2 && id==a3)
+// 							{
+						
+// 								alert("You won!");
+// 							}
+// else{
+
+// 	checkEmpty();//check whether all spaces filled up and then return you lost;
+// }
+// }
+// }
+
+// window.onload=function(){
+
+// 	// what to do when we recieve the request
+// var responseHandler = function() {
+//   console.log("response text", this.responseText);
+//   console.log("status text", this.statusText);
+//   console.log("status code", this.status);
+// };
+
+// var doSubmit = function(event)
+// { 
+// 	var input = document.querySelector('#url');
+// 	 var url = input.value; };
+
+// // make a new request
+// var request = new XMLHttpRequest();
+
+// // listen for the request response
+// request.addEventListener("load", responseHandler);
+
+// // ready the system by calling open, and specifying the url
+// request.open("GET", "https://swapi.co/api/people/1");
+// // send the request
+// request.send();
+// var responseHandler = function() {
+//   console.log("response text", this.responseText);
+//   var response = JSON.parse( this.responseText );
+//   console.log( response );
+// };
+
+// var requestFailed = function(){
+//   console.log("response text", this.responseText);
+//   console.log("status text", this.statusText);
+//   console.log("status code", this.status);
+// }
+// request.addEventListener("error", requestFailed);
+// document.querySelector('#submit').addEventListener('click', doSubmit);
+// }
+// // request.addEventListener("error", requestFailed);
+// // intit()
+
+// // }
