40f707ce7b9fa96419cf3620693cda7b37061e5d
lamesensei <16118606+lamesensei@users.noreply.github.com>
2018-09-11 22:30:57 +0800

    Add restart button

--- script.js
+++ script.js
@@ -2,63 +2,77 @@ var body = document.getElementsByTagName('body')[0]
 var field
 var square
 var display
+var button
 var turn = 0
-var winningCombination = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-    [1, 4, 7],
-    [2, 5, 8],
-    [3, 6, 9],
-    [1, 5, 9],
-    [3, 5, 7],
-];
+// var winningCombination = [
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9],
+//     [1, 4, 7],
+//     [2, 5, 8],
+//     [3, 6, 9],
+//     [1, 5, 9],
+//     [3, 5, 7],
+// ];
+
+function removeListeners() {
+    var board = document.getElementsByClassName('board')
+    for (i in board) {
+        board[i].removeEventListener('click', placeMark, false)
+    }
+}
 
 function checkWin(className) {
     var board = document.getElementsByClassName('board')
-    debugger;
     switch (true) {
         case (board[0].classList[1] === className && board[1].classList[1] === className && board[2].classList[1] === className):
             display.textContent = `${className} Wins!`
+            removeListeners()
             break;
         case (board[3].classList[1] === className && board[4].classList[1] === className && board[5].classList[1] === className):
             display.textContent = `${className} Wins!`
+            removeListeners()
             break;
         case (board[6].classList[1] === className && board[7].classList[1] === className && board[8].classList[1] === className):
             display.textContent = `${className} Wins!`
+            removeListeners()
             break;
         case (board[0].classList[1] === className && board[3].classList[1] === className && board[6].classList[1] === className):
             display.textContent = `${className} Wins!`
+            removeListeners()
             break;
         case (board[1].classList[1] === className && board[4].classList[1] === className && board[7].classList[1] === className):
             display.textContent = `${className} Wins!`
+            removeListeners()
             break;
         case (board[2].classList[1] === className && board[5].classList[1] === className && board[8].classList[1] === className):
             display.textContent = `${className} Wins!`
+            removeListeners()
             break;
         case (board[0].classList[1] === className && board[4].classList[1] === className && board[8].classList[1] === className):
             display.textContent = `${className} Wins!`
+            removeListeners()
             break;
         case (board[2].classList[1] === className && board[4].classList[1] === className && board[6].classList[1] === className):
             display.textContent = `${className} Wins!`
+            removeListeners()
             break;
         default:
-            display.textContent = 'Status'
+            display.textContent = 'In Progress'
     }
 
 }
 
 function placeMark(event) {
+    button.style.visibility = 'visible'
     if (turn % 2 == 0) {
         this.textContent = 'X'
         this.classList.add('X')
-        if (turn > 3)
-            checkWin('X')
+        checkWin('X')
     } else {
         this.textContent = 'O'
         this.classList.add('O')
-        if (turn > 3)
-            checkWin('O')
+        checkWin('O')
     }
     turn++
     event.target.removeEventListener('click', placeMark, false)
@@ -75,7 +89,7 @@ function createBoard(size) {
         square = document.createElement('div')
         square.setAttribute('id', i)
         square.classList = 'board'
-        square.innerHTML = i
+        square.innerHTML = '*'
         //square.style.border = 'solid 1px'
         //square.style.justifySelf = 'center'
         //square.style.alignSelf = 'center'
@@ -85,11 +99,23 @@ function createBoard(size) {
 
     body.appendChild(field)
     display = document.createElement('h1')
-    display.innerHTML = 'Status'
+    display.innerHTML = 'Ready'
     //display.style.border = 'solid 1px'
     //display.style.gridColumn = '1 / span 3'
     //display.style.justifySelf = 'center'
+    button = document.createElement('button')
+    button.innerHTML = 'Restart'
+    button.addEventListener('click', resetBoard)
+    button.style.visibility = 'hidden'
     body.appendChild(display)
+    body.appendChild(button)
+}
+
+function resetBoard() {
+    while (body.lastChild) {
+        body.removeChild(body.lastChild)
+    }
+    createBoard(9)
 }
 
 createBoard(9)
