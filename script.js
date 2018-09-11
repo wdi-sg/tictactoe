1bec5e2744dbe0c80b4b8b51fe95b35c251e97b9
shroomcrumpet <kencheng3@gmail.com>
2018-09-11 19:20:59 +0800

    Cleaner code

--- script.js
+++ script.js
@@ -23,14 +23,20 @@ function winCheck() {
     var dia1 = allButtons[0].textContent + allButtons[4].textContent + allButtons[8].textContent;
     var dia2 = allButtons[2].textContent + allButtons[4].textContent + allButtons[6].textContent;
 
-    if (row1 === "xxx" || row2 === "xxx" || row3 === "xxx" || col1 === "xxx" || col2 === "xxx" || col3 === "xxx" || dia1 === "xxx" || dia2 === "xxx") {
-        alert ("Player 'x' wins!");
-    } else if (row1 === "ooo" || row2 === "ooo" || row3 === "ooo" || col1 === "ooo" || col2 === "ooo" || col3 === "ooo" || dia1 === "ooo" || dia2 === "ooo") {
-        alert ("Player 'o' wins!");
-    } else alert ("Game is drawn.");
-}
+    var trios = [row1, row2, row3, col1, col2, col3, dia1, dia2];
 
-for(var i = 0; i < allButtons.length; i++){
+    for (i in trios) {
+
+        if (trios[i] === "xxx") {
+            alert ("Player 'x' wins!");
+        } else if (trios[i] === "ooo") {
+            alert ("Player 'o' wins!");
+        };
+    };
+};
+
+
+for (var i = 0; i < allButtons.length; i++){
 
     allButtons[i].addEventListener("click", function() {
         changePlayer();
