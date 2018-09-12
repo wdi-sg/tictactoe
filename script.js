3015a5b9867bbcf2f3983b0933313c7417679729
xnithunx <nithun.nanda@gmail.com>
2018-09-12 12:32:42 +0800

    added basic logic/loops

---
+++ script.js
@@ -0,0 +1,18 @@
+var playerOne = prompt("Welcome! What's your name Player One?");
+var playerOneSymbol = prompt("Choose your symbol\nx or o");
+var playerTwo = prompt("What's your name Player 2?");
+
+
+ if (player1Choice === 'x') {
+     var player2Choice = alert("Your symbol is: o");
+     player2Choice = 'o'
+
+
+ } else if (player1Choice ==='o'){
+     player2Choice = alert("Your symbol is: x");
+     player2Choice = 'x';
+
+
+ } else{
+
+ }
