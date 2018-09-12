52c396f8634f38a70945fe57e9681f5798cf223e
JonathanL1211 <Jonathanknight1211@gmail.com>
2018-09-12 20:09:25 +0800

    Added additional color to display which player won

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-function getWinner(){
+var turn = document.getElementById('turn');
+
+function selectWinnerBox (b1,b2,b3) {
+    b1.classList.add("win");
+    b2.classList.add("win");
+    b3.classList.add("win");
+    turn.innerHTML = b1.innerHTML + " Won Congrats";
+}
+
+function checkWinner(){
     var sq1 = document.getElementById('square1');
     var sq2 = document.getElementById('square2');
     var sq3 = document.getElementById('square3');
@@ -10,50 +19,65 @@ function getWinner(){
     var sq9 = document.getElementById('square9');
     //list all possibilities to win
     if(sq1.innerHTML !== "" && (sq1.innerHTML === sq2.innerHTML) && (sq1.innerHTML === sq3.innerHTML)){
-        console.log("win");
+        selectWinnerBox(sq1, sq2, sq3);
     }
     else if(sq4.innerHTML !== "" && (sq4.innerHTML === sq5.innerHTML) && (sq4.innerHTML === sq6.innerHTML)){
-        console.log("win");
+        selectWinnerBox(sq4, sq5, sq6);
     }
     else if(sq7.innerHTML !== "" && (sq7.innerHTML === sq8.innerHTML) && (sq7.innerHTML === sq9.innerHTML)){
-        console.log("win");
+        selectWinnerBox(sq7, sq8, sq9);
     }
     else if(sq1.innerHTML !== "" && (sq1.innerHTML === sq4.innerHTML) && (sq1.innerHTML === sq7.innerHTML)){
-        console.log("win");
+        selectWinnerBox(sq1, sq4, sq7);
     }
     else if(sq2.innerHTML !== "" && (sq2.innerHTML === sq5.innerHTML) && (sq2.innerHTML === sq8.innerHTML)){
-        console.log("win");
+        selectWinnerBox(sq2, sq5, sq8);
     }
     else if(sq3.innerHTML !== "" && (sq3.innerHTML === sq6.innerHTML) && (sq3.innerHTML === sq9.innerHTML)){
-        console.log("win");
+        selectWinnerBox(sq3, sq6, sq9);
     }
     else if(sq1.innerHTML !== "" && (sq1.innerHTML === sq5.innerHTML) && (sq1.innerHTML === sq9.innerHTML)){
-        console.log("win");
+        selectWinnerBox(sq1, sq5, sq9);
     }
     else if(sq3.innerHTML !== "" && (sq3.innerHTML === sq5.innerHTML) && (sq3.innerHTML === sq7.innerHTML)){
-        console.log("win");
+        selectWinnerBox(sq3, sq5, sq7);
     }
 }
+//Set event and winning condition
 var counter = 0;
-var boxes = document.querySelectorAll(".row div");
+var boxes = document.querySelectorAll("#main div");
 console.log(boxes);
 for (var i = 0; i < boxes.length; i++){
     boxes[i].onclick = function(){
-        if (counter % 2 === 0){
-            console.log(counter);
-            this.innerHTML = "X";
-            getWinner();
-            counter += 1;
+        //not allow to change the value of the box
+        if(this.innerHTML !== "X" && this.innerHTML !== "O"){
+            if (counter % 2 === 0){
+                console.log(counter);
+                this.innerHTML = "X";
+                turn.innerHTML = "It is O player turn";
+                checkWinner();
+                counter += 1;
+            }
+            else{
+                console.log(counter);
+                this.innerHTML = "O";
+                turn.innerHTML = "It is X player turn";
+                checkWinner();
+                counter += 1;
+            }
         }
-        else{
-            console.log(counter);
-            this.innerHTML = "O";
-            getWinner();
-            counter += 1;
-        }
+    }
+}
+
+function restart(){
+    for (var i = 0; i < boxes.length; i++){
+        boxes[i].classList.remove("win");
+        boxes[i].innerHTML = "";
+        turn.innerHTML = "Play";
+        turn.style.fontSize = "25px";
     }
 }
 
 
 
-getWinner();
+
