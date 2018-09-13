590baa0cf52108afc324832247da828805d97b65
weiwenl <weiwenl.justin@gmail.com>
2018-09-13 09:06:15 +0800

    recommit work from branch to HEAD, done win condition; but the reset button is not fixed

--- script.js
+++ script.js
@@ -1,24 +1,72 @@
-var container = document.querySelector("#container");
+/*Pseudo-code
+1)Build the board - Use HTML
+2)Play the game
+3)Keep track of whose turn
+4)Create players
+5)Win algorithmn
+6)Reset the game*/
 
-var board = [
-  [0, 0, 0],
-  [0, 0, 0],
-  [0, 0, 0]
-];
+//Create players
+var players = [];
+var markers = ["X", "O"]; //markers[0] will be "X"; markers[1] will be "O"
 
-var space = 100;
-var size = 10;
-var createBoard = function(){
-  for(var row = 0; row < board.length; row++){
-    for(var col = 0; col < board[0].length; col++){
-      var square = document.createElement("div");
-      square.setAttribute("class", "square");
-      container.appendChild(square);
+//Players name
+players[0] = "Ron"; //players[O] = player1;
+players[1] = "Jack"; //players[1] = player2;
+//var player1 = prompt("Enter first player's name:");
+//var player2 = prompt("Enter second player's name:");
 
-      square.style.top = row * (space + size) +"px";
-      square.style.left = col * (space + size) +"px" ;
+//Win Condition
+var winArray = [7, 56, 73, 84, 146, 273, 292, 448];
+var playerValues = [0, 0];
+var gameOver = false;
+
+var turn = 0;
+//Play the game
+var play = function(click, divValue){
+  if(gameOver !== true){
+    click.onclick = "";
+    cellValueCount(divValue);
+    click.innerText = markers[turn];
+    checkForWin();
+    if(gameOver !== true){
+      togglePlayer();
     }
   }
 }
 
-createBoard();
+//Keep track of whose turn
+var togglePlayer = function(){
+  if(turn === 0){
+    turn = 1;
+  }
+  else{
+    turn = 0;
+  }
+  document.getElementById("gameMessage").innerText = players[turn] + "'s turn'"
+}
+
+//Win algorithmn - Check for the win
+//  1  2   4    => 7
+//  8  16  32   => 56
+// 64 128 256   => 73
+
+// 84 146 273    292, 448   Total num value is:  511.
+
+//Wherever player clicks he scores a value and it adds up
+var cellValueCount = function(divValue){
+  playerValues[turn] += divValue;
+}
+
+var checkForWin = function(){
+  for(var i = 0; i < winArray.length; i++){
+    if((winArray[i] & playerValues[turn]) === winArray[i]){
+      gameOver = true;
+      document.getElementById("gameMessage").innerText = players[turn] + " wins!";
+    }
+  }
+  if(((playerValues[0] + playerValues[1]) === 511) && (gameOver === false)){
+    gameOver = true;
+    document.getElementById("gameMessage").innerText = "It's a draw!";
+  }
+}
