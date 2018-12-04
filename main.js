a2f3e39a20e187a2971fdd01a8188fed8a13a583
Your Name <ou@example.com>
2018-12-04 16:45:16 +0800

    overhaul to html and css. left mainly js logic for determining winner

--- main.js
+++ main.js
@@ -4,14 +4,18 @@ var turn = 0;
 
 var addX = function(event) {
     event.target.textContent="x";
+    event.target.style.color="#003366";
 }
 
 var addO = function(event) {
     event.target.textContent="o";
+    event.target.style.color="#8b0000";
 }
 
 var addEntry = function(event) {
-    if (turn%2==0) {
+    if (turn >= 9) {
+        alert("The game has ended");
+    }   else if (turn%2==0) {
         addX(event);
         turn++;
     }   else   {
