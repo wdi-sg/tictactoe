f25fb19c9ac9d557a905c6a488c47cbb5372110b
Ishak <ishakbhn@gmail.com>
2018-12-05 22:40:15 +0800

    determine the winner

--- script.js
+++ script.js
@@ -7,27 +7,97 @@
 
 var turn = null;
 
+var square =["*","*","*","*","*","*","*","*","*"];
+
+var winArray = [
+[0,1,2],
+[3,4,5],
+[6,7,8],
+[0,3,6],
+[1,4,7],
+[2,5,8],
+[0,4,8],
+[2,4,6]];
 
 var play = function () {
 
-if(this.textContent.length === 0){
-        if (turn === null || turn === 0){
-            this.textContent ="X";
-            turn += 1;
-        } else {
-            this.textContent = "O";
-            turn -= 1;
-        } return turn;
+    if(this.textContent === ""){
+            if (turn === null || turn === 0){
+                this.textContent ="X";
+                turn += 1;
+
+                checkBox()
+            } else {
+                this.textContent = "O";
+                turn -= 1;
+                checkBox()
+            }
+            return turn;
+    } else {
+ }
+
+};
+console.log("come out of play function after else; return.")
+
+var checkBox = function() {
+
+
+    var allButtons = document.querySelectorAll("button");
+
+    allButtons.forEach(function(element, index){
+
+            if(allButtons[index].innerHTML  === 'X' || allButtons[index].innerHTML === 'O')
+
+        square[index] = allButtons[index].innerHTML;
+
+    }); checkWin()
+
+}
+
+var checkWin = function() {
+
+    if ( square[0] === 'X' &&  square[1] === 'X' && square[2] === 'X' ) {
+        alert("X win!");
+        stop();
+    } else if ( square[0] === 'O' &&  square[1] === 'O' && square[2] === 'O') {
+        alert("O win!");
+    } else if ( square[3] === 'X' &&  square[4] === 'X' && square[5] === 'X') {
+       alert("X win!");
+    } else if ( square[3] === 'O' &&  square[4] === 'O' && square[5] === 'O') {
+        alert("O win!");
+    } else if ( square[6] === 'X' &&  square[7] === 'X' && square[8] === 'X') {
+       alert("X win!");
+    } else if ( square[6] === 'O' &&  square[7] === 'O' && square[8] === '0') {
+       alert("O win!");
+    } else if ( square[0] === 'X' &&  square[3] === 'X' && square[6] === 'X') {
+       alert("X win!");
+    } else if ( square[0] === 'O' &&  square[3] === 'O' && square[6] === 'O') {
+       alert("O win!");
+    } else if ( square[1] === 'X' &&  square[4] === 'X' && square[7] === 'X') {
+       alert("X win!");
+    } else if ( square[1] === 'O' &&  square[4] === 'O' && square[7] === 'O') {
+       alert("O win!");
+    } else if ( square[2] === 'X' &&  square[5] === 'X' && square[8] === 'X') {
+       alert("X win!");
+    } else if ( square[2] === '0' &&  square[5] === '0' && square[8] === '0') {
+       alert("O win!");
+    } else if ( square[0] === 'X' &&  square[4] === 'X' && square[8] === 'X') {
+       alert("X win!");
+    } else if ( square[0] === 'O' &&  square[4] === 'O' && square[8] === 'O') {
+       alert("O win!");
+    } else if ( square[2] === 'X' &&  square[4] === 'X' && square[6] === 'X') {
+       alert("X win!");
+    } else if ( square[2] === '0' &&  square[4] === '0' && square[6] === 'X') {
+       alert("O win!");
     }
 }
 
-// var checkBox = function () {
-
-// if()
-
+var stop = function(){
+//removeEventListener("mousemove", myFunction)
 }
 
 window.onload = function(){
+
 
      var getButtons = document.querySelectorAll("button");
      // set an event listener on each element
