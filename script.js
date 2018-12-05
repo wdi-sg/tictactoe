7fc823357b11caf267e342ce25d77b6bb1f7363f
angrylobster <liewminshan@gmail.com>
2018-12-05 16:29:58 +0800

    Players can choose symbol and name

--- script.js
+++ script.js
@@ -1,28 +1,53 @@
 const winningMoves = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+const colors = ["red", "orange", "green", "purple", "brown", "blue"]
 const cells = document.body.querySelectorAll(".cell") //this is a NodeList of cells
 const restartButton = document.body.querySelector("#restart-button")
 
 var turn = 1
-var playerOne = generatePlayer("X", "red")
-var playerTwo = generatePlayer("O", "blue")
+var playerOne = generatePlayer("one")
+var playerTwo = generatePlayer("two")
 var currentPlayer
 
 startGame()
 
 // -------------------------------------------------------------------------
 
-function generatePlayer(icon, color){
-    var playerObject = {
+function generatePlayer(number){
+    var name
+    var symbol
+    while (true){
+        name = prompt("Hello player " + number + "! What's your name?")
+        if (name !== ""){
+            break
+        } else {
+            alert("You must enter a name!")
+        }
+    }
+    while (true){
+        symbol = prompt("Very well, " + name + ". What symbol do you choose?")
+        if (symbol.length > 1){
+            alert("You can only have a single character for your symbol!")
+        } else {
+            break
+        }
+    }
+    let color = colors.splice(Math.floor(Math.random() * (colors.length-1)),1)
+    return playerObject(name,symbol,color)
+}
+
+function playerObject(name, icon, color){
+    var object = {
+        'name': name,
         'icon': icon,
         'color': color,
         'moves': []
     }
-    return playerObject
+    return object
 }
 
 function startGame(){
     for(let cell of cells){
-        cell.addEventListener("click", turnStart)
+        cell.addEventListener("click", playTurn)
     }
     restartButton.addEventListener("click", restart)
 }
@@ -34,21 +59,26 @@ function restart(){
     startGame()
 }
 
-function turnStart(){
-    playTurn(this)
-    if (turn === 1){
-        restartButton.style.visibility = "hidden"
+function playTurn(){
+    if (this.innerText === ""){
+        if (turn === 1){
+            restartButton.style.visibility = "hidden"
+        }
+        changeCell(this)
+        turnPlayed()
     }
-    if (turn>=5){
-        checkVictory()
-    }
-    if (turn === 9){
-        gameDraw()
-    }
-    turn++
 }
 
-function playTurn(object){
+function turnPlayed(){
+    turn++
+    if (turn>=5 && somebodyWon()){
+        gameWon()
+    } else if (turn === 10 && !somebodyWon()){
+        gameDraw()
+    }
+}
+
+function changeCell(object){
     currentPlayer = getCurrentPlayer()
     object.innerText = currentPlayer.icon
     object.style.color = currentPlayer.color
@@ -63,14 +93,14 @@ function getCurrentPlayer(){
     }
 }
 
-function checkVictory(){
+function somebodyWon(){
     let currentPlayerMoves = currentPlayer.moves
     for (let set of winningMoves){
         if (set.every(number => {
             return currentPlayerMoves.indexOf(number) > -1
         })){
-            gameWon(set)
-            break
+            showWinningCells(set)
+            return true
         }
     }
 }
@@ -81,9 +111,8 @@ function showWinningCells(set){
     }
 }
 
-function gameWon(set){
+function gameWon(){
     clearClickListeners()
-    showWinningCells(set)
     restartButton.style.visibility = ""
 }
 
@@ -108,6 +137,6 @@ function resetCells(){
 
 function clearClickListeners(){
     for (let cell of cells){
-        cell.removeEventListener("click", turnStart)
+        cell.removeEventListener("click", playTurn)
     }
 }
