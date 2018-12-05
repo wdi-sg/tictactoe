c937ded1dd686c313a8c2fab7e674f174e8f934d
Andrea <andrealmj@live.com>
2018-12-05 20:40:06 +0800

    fixed bugs, added detection of win state as well as a refresh button and scorekeeping system

--- script.js
+++ script.js
@@ -1,30 +1,36 @@
 //Define global variables
 //var allButtons = document.querySelectorAll(".grid-container");
-var button = document.querySelectorAll(".grid-item");
+var button = document.querySelectorAll("button");
 
 var buttonCounter = 0;
 
-
 //Define function - what happens when a button is clicked?
 var clicked = function() {
-    buttonCounter++; //buttonCounter to determine whether or not button text content shows X or O
-    console.log(buttonCounter);
 
-    if (buttonCounter >= 10) {
-        alert("Thanks for playing! Please refresh the page to play again.");
-    } else if (buttonCounter === 0 || buttonCounter%2 !== 0) {
-        this.textContent = "X";
-    } else {
-        this.textContent = "O";
+    //console.log(buttonCounter);
+    if (this.textContent === " ") {
+
+            buttonCounter++; //buttonCounter to determine whether or not button text content shows X or O
+            console.log("buttonCounter if the clicked button was previously blank: "+buttonCounter);
+
+
+            //Lines 18 and 19 were put here in an earlier version of the code to ensure that once all buttons were filled with either X or O, the game could not continue. It is not necessary anymore and will therefore be commented out. (buttonCounter will now never >= 10 without either the DRAW message displaying or a player winning (& therefore the winMessage displaying).)
+            // if (buttonCounter >= 10) {
+            //     alert("Thanks for playing! Please click on the refresh button at the bottom of the page to play again.");}
+            if (buttonCounter%2 !== 0) {
+                this.textContent = "X";
+            } else {
+                this.textContent = "O";
+            }
+        checkForWin();
     }
-};
-
-//On 'click', perform function 'clicked'
-for (var i = 0; i < button.length; i++) {
-    button[i].addEventListener("click", clicked)
 }
 
-
+//On 'click', perform function 'clicked'
+//Set condition i<9 so as to (upon 'click') execute this function for all buttons EXCLUDING the refresh button (which has an index of [9])
+for (var i = 0; i < 9; i++) {
+    button[i].addEventListener("click", clicked)
+}
 
 
 //Detecting a win state
@@ -40,6 +46,9 @@ var box7 = document.querySelector("#box7");
 var box8 = document.querySelector("#box8");
 var box9 = document.querySelector("#box9");
 
+//Define variables to store players' scores:
+let playerXScore = 0;
+let playerOScore = 0;
 
 //define function to check for win state:
   //     |   |
@@ -50,37 +59,114 @@ var box9 = document.querySelector("#box9");
   //   7 | 8 | 9
   //     |   |
 
-var checkForWin = function () {
-    if (
-        ((box1.textContent === box2.textContent) && (box2.textContent === box3.textContent))
-        // || ((box4.textContent === box5.textContent) && (box5.textContent=== box6.textContent))
-        // || ((box7.textContent === box8.textContent) && (box8.textContent === box9.textContent))
-        // || ((box1.textContent === box4.textContent) && (box4.textContent === box7.textContent))
-        // || ((box2.textContent === box8.textContent) && (box8.textContent === box5.textContent))
-        // || ((box3.textContent === box6.textContent) && (box6.textContent === box9.textContent))
-        // || ((box7.textContent === box3.textContent) && (box3.textContent === box5.textContent))
-        // || ((box1.textContent === box5.textContent) && (box5.textContent === box9.textContent))
-        )
+var checkForWin = function() {
 
-    {
-    winMessage();
-    } else {
-        alert("It's a draw!");
-    }
+//Hard-code all possible win states
+        if ( (box1.textContent !== " ") && (box1.textContent === box2.textContent) && (box2.textContent === box3.textContent) ) {
+            if (box1.textContent === "X") {
+                playerXScore++;
+                winMessage("X");
+            } else {
+                playerOScore++;
+                winMessage("O");
+            }
+        }
+        else if ( (box4.textContent !== " ") && (box4.textContent === box5.textContent) && (box5.textContent=== box6.textContent) ) {
+            if (box4.textContent === "X") {
+                playerXScore++;
+                winMessage("X");
+            } else {
+                playerOScore++;
+                winMessage("O");
+            }
+        }
+        else if ( (box7.textContent !== " ") && (box7.textContent === box8.textContent) && (box8.textContent === box9.textContent) ) {
+            if (box7.textContent === "X") {
+                playerXScore++;
+                winMessage("X");
+            } else {
+                playerOScore++;
+                winMessage("O");
+            }
+        }
+        else if ( (box1.textContent !== " ") && (box1.textContent === box4.textContent) && (box4.textContent === box7.textContent) ) {
+            if (box1.textContent === "X") {
+                playerXScore++;
+                winMessage("X");
+            } else {
+                playerOScore++;
+                winMessage("O");
+            }
+        }
+        else if ( (box2.textContent !== " ") && (box2.textContent === box8.textContent) && (box8.textContent === box5.textContent) ) {
+            if (box2.textContent === "X") {
+                playerXScore++;
+                winMessage("X");
+            } else {
+                playerOScore++;
+                winMessage("O");
+            }
+        }
+        else if ( (box3.textContent !== " ") && (box3.textContent === box6.textContent) && (box6.textContent === box9.textContent) ) {
+            if (box3.textContent === "X") {
+                playerXScore++;
+                winMessage("X");
+            } else {
+                playerOScore++;
+                winMessage("O");
+            }
+        }
+        else if ( (box7.textContent !== " ") && (box7.textContent === box3.textContent) && (box3.textContent === box5.textContent) ) {
+            if (box7.textContent === "X") {
+                playerXScore++;
+                winMessage("X");
+            } else {
+                playerOScore++;
+                winMessage("O");
+            }
+        }
+        else if ( (box1.textContent !== " ") && (box1.textContent === box5.textContent) && (box5.textContent === box9.textContent) ) {
+            if (box1.textContent === "X") {
+                playerXScore++;
+                winMessage("X");
+            } else {
+                playerOScore++;
+                winMessage("O");
+            }
+        } //What happens if no winner
+         else if (buttonCounter === 9) {
+            alert("It's a draw!\n\nPlayer scores:\n\nPlayer X: "+playerXScore+"\nPlayer O: "+playerOScore+"\n\nThanks for playing. Please click on the refresh button at the bottom of the page to play again.");
+            playerXScore = playerXScore;
+            playerOScore = playerOScore;
+        }
 }
-
 
 //What to do when win state is achieved:
-var winMessage = function () {
-    alert("Congratulations! Player ?? has won.");
+var winMessage = function(name) {
+    alert("Congratulations! Player "+name+" won!\n\nPlayer scores:\n\nPlayer X: "+playerXScore+"\nPlayer O: "+playerOScore+"\n\nThanks for playing. Please click on the refresh button at the bottom of the page to play again.");
 }
 
-//checkForWin();
+//Setting up "refresh" of page
+
+//assign var to refresh button
+var refreshButton = document.querySelector("#refresh-button");
+
+//Define reset function to set button text content to " " and reset buttonCounter to 0:
+var reset = function() {
+
+    //Similar to what was written in line 31, set condition to i < 9 so that button[i].textContent = " " is only done to buttons 1 to 9 (and NOT the refresh button) upon clicking.
+    for (var i = 0; i < 9; i++) {
+        button[i].textContent = " ";
+    }
+
+    buttonCounter = 0;
+
+    return;
+};
+
+//On click (refreshButton), perform reset function
+refreshButton.addEventListener("click", reset);
 
 
-//Hard-coding for win states:
 
 
-// window.onload = function () {
-//     checkForWin();
-// }
