cb7f402b62b7b8a4a459d6b5a5073420d6a9226c
Moses Lee <moseslee90@gmail.com>
2019-03-20 02:06:15 +0800

    setup basic graphical interface for game

--- main.js
+++ main.js
@@ -1,11 +1,48 @@
 var gameBoard = [];
 var gameSize = 3;
 var winCon = 3;
+var boxClicked = function () {
+    var x = parseInt(this.getAttribute("data-x"));
+    var y = parseInt(this.getAttribute("data-y"));
+    var state = parseInt(this.getAttribute("data-state"));
+    console.log("User clicked on box " + x + " " + y);
+    var currentText = this.innerText;
+    if (!(currentText === "X" || currentText === "O")) {
+        var oneOrTwo = circleCross();
+        var letter = void 0;
+        if (oneOrTwo === 1) {
+            //return an X
+            letter = "X";
+        }
+        else {
+            //return an O
+            letter = "O";
+        }
+        this.childNodes[0].innerText = letter;
+        //assign that oneOrTwo to the array representation
+        gameBoard[x][y] = oneOrTwo;
+        checkWin();
+    }
+};
 function setupBoard() {
+    //create a boxClicked function that extracts the id from data-x and data-y and parses it back into numbers to reference to our checkWin function.
+    var gameBoardHTML = document.getElementById("game-board");
+    var gameBoardDimension = gameSize * 54;
+    gameBoardHTML.style.width = gameBoardDimension + "px";
+    gameBoardHTML.style.height = gameBoardDimension + "px";
     for (var i = 0; i < gameSize; i++) {
         var array2d = [];
         for (var j = 0; j < gameSize; j++) {
             array2d.push(0);
+            var element = document.createElement("div");
+            var paragraph = document.createElement("p");
+            element.setAttribute("data-x", i.toString());
+            element.setAttribute("data-y", j.toString());
+            element.setAttribute("data-state", "0");
+            element.setAttribute("class", "box");
+            element.appendChild(paragraph);
+            element.addEventListener("click", boxClicked);
+            gameBoardHTML.appendChild(element);
         }
         gameBoard.push(array2d);
     }
@@ -18,7 +55,7 @@ function checkHorizontal(x, y) {
     var winCountX = 0;
     if (horizontalSpace >= winCon) {
         for (var i = 0; i < winCon; i++) {
-            if (gameBoard[x][y] === gameBoard[x + i][y] && (gameBoard[x][y] != 0)) {
+            if (gameBoard[x][y] === gameBoard[x + i][y] && gameBoard[x][y] != 0) {
                 winCountX++;
             }
         }
@@ -32,7 +69,7 @@ function checkVertical(x, y) {
     var winCountY = 0;
     if (verticalSpace >= winCon) {
         for (var i = 0; i < winCon; i++) {
-            if (gameBoard[x][y] === gameBoard[x][y + i] && (gameBoard[x][y] != 0)) {
+            if (gameBoard[x][y] === gameBoard[x][y + i] && gameBoard[x][y] != 0) {
                 winCountY++;
             }
         }
@@ -44,11 +81,20 @@ function checkVertical(x, y) {
 function checkDiagonal(x, y) {
     var horizontalSpace = horizontalLength - x;
     var verticalSpace = verticalLength - y;
-    var diagonalSpace = Math.min(horizontalSpace, verticalSpace);
+    var diagonalSpaceUp = Math.min(horizontalSpace, verticalSpace);
     var winCountXY = 0;
-    if (diagonalSpace >= winCon) {
+    if (diagonalSpaceUp >= winCon) {
         for (var i = 0; i < winCon; i++) {
-            if (gameBoard[x][y] === gameBoard[x + i][y + i] && (gameBoard[x][y] != 0)) {
+            if (gameBoard[x][y] === gameBoard[x + i][y + i] && gameBoard[x][y] != 0) {
+                winCountXY++;
+            }
+        }
+    }
+    var verticalSpaceDown = y + 1;
+    var diagonalSpaceDown = Math.min(horizontalSpace, verticalSpaceDown);
+    if (diagonalSpaceDown >= winCon) {
+        for (var i = 0; i < winCon; i++) {
+            if (gameBoard[x][y] === gameBoard[x + i][y - i] && gameBoard[x][y] != 0) {
                 winCountXY++;
             }
         }
@@ -68,3 +114,17 @@ function checkWin() {
     }
 }
 //write a function that alternates between returning the number 1 and the number 2 everytime it is called
+var circleOrCross = 0;
+function circleCross() {
+    circleOrCross++;
+    var oneOrTwo = 0;
+    if (circleOrCross % 2 === 0) {
+        //circleOrCross is odd
+        return 1;
+    }
+    else {
+        return 2;
+    }
+}
+//assign divs data-id attributes of data-id-x and data-id-y
+//generate them all with javascript
--- main.ts
+++ main.ts
@@ -1,11 +1,49 @@
 let gameBoard: number[][] = [];
 let gameSize: number = 3;
 let winCon: number = 3;
+
+var boxClicked = function() {
+  let x: number = parseInt(this.getAttribute("data-x"));
+  let y: number = parseInt(this.getAttribute("data-y"));
+  let state: number = parseInt(this.getAttribute("data-state"));
+  console.log("User clicked on box " + x + " " + y);
+  let currentText: string = this.innerText;
+  if (!(currentText === "X" || currentText === "O")) {
+    let oneOrTwo: number = circleCross();
+    let letter: string;
+    if (oneOrTwo === 1) {
+      //return an X
+      letter = "X";
+    } else {
+      //return an O
+      letter = "O";
+    }
+    this.childNodes[0].innerText = letter;
+    //assign that oneOrTwo to the array representation
+    gameBoard[x][y] = oneOrTwo;
+    checkWin();
+  }
+};
+
 function setupBoard() {
+  //create a boxClicked function that extracts the id from data-x and data-y and parses it back into numbers to reference to our checkWin function.
+  var gameBoardHTML = document.getElementById("game-board");
+  let gameBoardDimension: number = gameSize * 54;
+  gameBoardHTML.style.width = gameBoardDimension + "px";
+  gameBoardHTML.style.height = gameBoardDimension + "px";
   for (let i = 0; i < gameSize; i++) {
     let array2d: number[] = [];
     for (let j = 0; j < gameSize; j++) {
       array2d.push(0);
+      var element = document.createElement("div");
+      var paragraph = document.createElement("p");
+      element.setAttribute("data-x", i.toString());
+      element.setAttribute("data-y", j.toString());
+      element.setAttribute("data-state", "0");
+      element.setAttribute("class", "box");
+      element.appendChild(paragraph);
+      element.addEventListener("click", boxClicked);
+      gameBoardHTML.appendChild(element);
     }
     gameBoard.push(array2d);
   }
@@ -20,7 +58,7 @@ function checkHorizontal(x: number, y: number) {
   let winCountX = 0;
   if (horizontalSpace >= winCon) {
     for (let i = 0; i < winCon; i++) {
-      if (gameBoard[x][y] === gameBoard[x + i][y] && (gameBoard[x][y] != 0) ) {
+      if (gameBoard[x][y] === gameBoard[x + i][y] && gameBoard[x][y] != 0) {
         winCountX++;
       }
     }
@@ -34,7 +72,7 @@ function checkVertical(x: number, y: number) {
   let winCountY = 0;
   if (verticalSpace >= winCon) {
     for (let i = 0; i < winCon; i++) {
-      if (gameBoard[x][y] === gameBoard[x][y + i] && (gameBoard[x][y] != 0) ) {
+      if (gameBoard[x][y] === gameBoard[x][y + i] && gameBoard[x][y] != 0) {
         winCountY++;
       }
     }
@@ -46,11 +84,20 @@ function checkVertical(x: number, y: number) {
 function checkDiagonal(x: number, y: number) {
   let horizontalSpace: number = horizontalLength - x;
   let verticalSpace: number = verticalLength - y;
-  let diagonalSpace: number = Math.min(horizontalSpace, verticalSpace);
+  let diagonalSpaceUp: number = Math.min(horizontalSpace, verticalSpace);
   let winCountXY = 0;
-  if (diagonalSpace >= winCon) {
+  if (diagonalSpaceUp >= winCon) {
     for (let i = 0; i < winCon; i++) {
-      if (gameBoard[x][y] === gameBoard[x + i][y + i] && (gameBoard[x][y] != 0) ) {
+      if (gameBoard[x][y] === gameBoard[x + i][y + i] && gameBoard[x][y] != 0) {
+        winCountXY++;
+      }
+    }
+  }
+  let verticalSpaceDown: number = y + 1;
+  let diagonalSpaceDown: number = Math.min(horizontalSpace, verticalSpaceDown);
+  if (diagonalSpaceDown >= winCon) {
+    for (let i = 0; i < winCon; i++) {
+      if (gameBoard[x][y] === gameBoard[x + i][y - i] && gameBoard[x][y] != 0) {
         winCountXY++;
       }
     }
@@ -70,3 +117,16 @@ function checkWin() {
   }
 }
 //write a function that alternates between returning the number 1 and the number 2 everytime it is called
+let circleOrCross = 0;
+function circleCross() {
+  circleOrCross++;
+  let oneOrTwo = 0;
+  if (circleOrCross % 2 === 0) {
+    //circleOrCross is odd
+    return 1;
+  } else {
+    return 2;
+  }
+}
+//assign divs data-id attributes of data-id-x and data-id-y
+//generate them all with javascript
