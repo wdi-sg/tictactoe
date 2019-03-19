e912477547662192e35f2b4858a9c18be38728d4
Moses Lee <moseslee90@gmail.com>
2019-03-20 00:29:54 +0800

    created game logic for setting up board, and examining win condition met

---
+++ main.js
@@ -0,0 +1,69 @@
+var gameBoard = [];
+var gameSize = 3;
+var winCon = 3;
+function setupBoard() {
+    for (var i = 0; i < gameSize; i++) {
+        var array2d = [];
+        for (var j = 0; j < gameSize; j++) {
+            array2d.push(0);
+        }
+        gameBoard.push(array2d);
+    }
+}
+setupBoard();
+var horizontalLength = gameBoard.length;
+var verticalLength = gameBoard[0].length;
+function checkHorizontal(x, y) {
+    var horizontalSpace = horizontalLength - x;
+    var winCountX = 0;
+    if (horizontalSpace >= winCon) {
+        for (var i = 0; i < winCon; i++) {
+            if (gameBoard[x][y] === gameBoard[x + i][y]) {
+                winCountX++;
+            }
+        }
+    }
+    if (winCountX === winCon) {
+        console.log("horizontal win condition met");
+    }
+}
+function checkVertical(x, y) {
+    var verticalSpace = verticalLength - y;
+    var winCountY = 0;
+    if (verticalSpace >= winCon) {
+        for (var i = 0; i < winCon; i++) {
+            if (gameBoard[x][y] === gameBoard[x][y + i]) {
+                winCountY++;
+            }
+        }
+    }
+    if (winCountY === winCon) {
+        console.log("vertical win condition met");
+    }
+}
+function checkDiagonal(x, y) {
+    var horizontalSpace = horizontalLength - x;
+    var verticalSpace = verticalLength - y;
+    var diagonalSpace = Math.min(horizontalSpace, verticalSpace);
+    var winCountXY = 0;
+    if (diagonalSpace >= winCon) {
+        for (var i = 0; i < winCon; i++) {
+            if (gameBoard[x][y] === gameBoard[x + i][y + i]) {
+                winCountXY++;
+            }
+        }
+    }
+    if (winCountXY === winCon) {
+        console.log("diagonal win condition met");
+    }
+}
+function checkWin() {
+    //function to check through all 3 possible directions in each cell
+    for (var i = 0; i < horizontalLength; i++) {
+        for (var j = 0; j < verticalLength; j++) {
+            checkHorizontal(i, j);
+            checkVertical(i, j);
+            checkDiagonal(i, j);
+        }
+    }
+}
---
+++ main.ts
@@ -0,0 +1,71 @@
+let gameBoard: number[][] = [];
+let gameSize: number = 3;
+let winCon: number = 3;
+function setupBoard() {
+  for (let i = 0; i < gameSize; i++) {
+    let array2d: number[] = [];
+    for (let j = 0; j < gameSize; j++) {
+      array2d.push(0);
+    }
+    gameBoard.push(array2d);
+  }
+}
+setupBoard();
+
+let horizontalLength: number = gameBoard.length;
+let verticalLength: number = gameBoard[0].length;
+
+function checkHorizontal(x: number, y: number) {
+  let horizontalSpace: number = horizontalLength - x;
+  let winCountX = 0;
+  if (horizontalSpace >= winCon) {
+    for (let i = 0; i < winCon; i++) {
+      if (gameBoard[x][y] === gameBoard[x + i][y]) {
+        winCountX++;
+      }
+    }
+  }
+  if (winCountX === winCon) {
+    console.log("horizontal win condition met");
+  }
+}
+function checkVertical(x: number, y: number) {
+  let verticalSpace: number = verticalLength - y;
+  let winCountY = 0;
+  if (verticalSpace >= winCon) {
+    for (let i = 0; i < winCon; i++) {
+      if (gameBoard[x][y] === gameBoard[x][y + i]) {
+        winCountY++;
+      }
+    }
+  }
+  if (winCountY === winCon) {
+    console.log("vertical win condition met");
+  }
+}
+function checkDiagonal(x: number, y: number) {
+  let horizontalSpace: number = horizontalLength - x;
+  let verticalSpace: number = verticalLength - y;
+  let diagonalSpace: number = Math.min(horizontalSpace, verticalSpace);
+  let winCountXY = 0;
+  if (diagonalSpace >= winCon) {
+    for (let i = 0; i < winCon; i++) {
+      if (gameBoard[x][y] === gameBoard[x + i][y + i]) {
+        winCountXY++;
+      }
+    }
+  }
+  if (winCountXY === winCon) {
+    console.log("diagonal win condition met");
+  }
+}
+function checkWin() {
+  //function to check through all 3 possible directions in each cell
+  for (let i = 0; i < horizontalLength; i++) {
+    for (let j = 0; j < verticalLength; j++) {
+      checkHorizontal(i, j);
+      checkVertical(i, j);
+      checkDiagonal(i, j);
+    }
+  }
+}
