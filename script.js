e3d95e5b7051d6d67acb56a9f3e0ee3ab85b2028
kinskin <md.asshikin@gmail.com>
2019-07-02 17:47:35 +0800

    table done

---
+++ script.js
@@ -0,0 +1,27 @@
+var startGame = function(event){
+    console.log("Game Board created");
+}
+
+var tableCreate = function(){
+    var table = document.createElement("table");
+
+    for (let i = 0; i < 3; i++){
+        var tr = table.insertRow();
+        for (let j = 0; j < 3; j++){
+            if(i === 2 && j === 3){
+                break;
+            }
+            else{
+                var td = tr.insertCell();
+                td.setAttribute("id", j)
+                td.style.width = "200px";
+                td.style.height= "200px";
+                td.style.border = "1px solid black";
+                tr.appendChild(td);
+            }
+            table.appendChild(tr)
+        }
+    document.body.appendChild(table);
+    }
+}
+tableCreate();
