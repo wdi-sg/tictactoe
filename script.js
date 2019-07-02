f875013c31a86509f7da42dd25bebd62026c7e5d
irockmysock <benny_chin89@hotmail.com>
2019-07-02 22:48:50 +0800

    basic game logic complete

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 console.log("hello script js");
 
 var turn = 0;
-var playerOne = [1,2,3];
+var playerOne = [];
 var playerTwo = [];
 var winPerm = [
     [1,2,3],
@@ -13,16 +13,7 @@ var winPerm = [
     [1,5,9],
     [3,5,7]
 ];
-var winArray = [];
-
-for (var i=0; i<winPerm.length; i++) {
-    winPerm[i]
-}
-console.log(winPerm[0]);
-console.log(playerOne);
-if (winPerm[0] === playerOne) {
-    console.log("Playerone win")
-}
+// var winArray = [];
 
 var div1 = document.getElementById("1");
 var div2 = document.getElementById("2");
@@ -34,14 +25,36 @@ var div7 = document.getElementById("7");
 var div8 = document.getElementById("8");
 var div9 = document.getElementById("9");
 
-// var checkWin = function() {
-//     if (playerOne === )
+
+console.log(winPerm[0]);
+console.log(playerOne);
+
+
+// if ((winPerm[0].every(element => playerOne.includes(element)))=== true ) {
+//     console.log("fucntion works");
 // }
+
+
+
+var checkWin = function (element) {
+    for (i=0;i<winPerm.length;i++) {
+
+        if ((winPerm[i].every(element => playerOne.includes(element)))=== true ) {
+        console.log("playerOne Wins!");
+        alert("Player 1 Won")
+        } else if ((winPerm[i].every(element => playerOne.includes(element)))=== true ) {
+            console.log("playerTwo Wins!");
+            alert("Player 2 WON")
+        }
+    }
+}
+
+
 
 var changeText = function(event){
     if (turn === 0 || turn%2 === 0) {
         console.log("P1 clicked: "+ event.target.innerText);
-        playerOne.push(event.target.innerText);
+        playerOne.push(parseInt(event.target.innerText));
         console.log(playerOne);
         event.target.innerText = "X";
         turn++;
@@ -52,23 +65,15 @@ var changeText = function(event){
         console.log(playerTwo);
         event.target.innerText = "O";
         turn++;
+        checkWin
     }
+
 
 }
 
 
 
 document.addEventListener('DOMContentLoaded', function( event ){
-
-    // var div1 = document.getElementById("1");
-    // var div2 = document.getElementById("2");
-    // var div3 = document.getElementById("3");
-    // var div4 = document.getElementById("4");
-    // var div5 = document.getElementById("5");
-    // var div6 = document.getElementById("6");
-    // var div7 = document.getElementById("7");
-    // var div8 = document.getElementById("8");
-    // var div9 = document.getElementById("9");
 
     div1.addEventListener('click', changeText);
     div2.addEventListener('click', changeText);
