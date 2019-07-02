fc32e7c58f935f3903bdde98d80a2d4d524f1273
EliseChia <swedesurrender@gmail.com>
2019-07-02 23:17:23 +0800

    experimenting with using for loop to check rather than hardcode

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
+//all da variableessss
 var counter = 1;
 
-//Player 1 turn function
+var C1Array = [];
+var C2Array = [];
+
+//p1 turn function
 var playerOneTurn = function() {
     event.target.innerHTML = "X";
     counter += 1;
 };
 
-//Player 2 turn function
+//p2 turn function
 var playerTwoTurn = function() {
     event.target.innerHTML = "O";
     counter += 1;
 };
 
-//Main game-playing function
+//main game-playing function, conditional to check if P1 or P2 turn
 var playGame = function() {
     var oddOrEven = counter % 2;
     if (oddOrEven > 0) {
@@ -24,7 +28,7 @@ var playGame = function() {
 
 var gameSquares = [];
 
-//Adding event listener for click to each square
+//add event listener for click to each game square, triggers playGame() function
 var initialiseSquares = function() {
     gameSquares = document.querySelectorAll("section")
     for (var i = 0; i < gameSquares.length; i++) {
@@ -33,4 +37,56 @@ var initialiseSquares = function() {
 }
 initialiseSquares();
 
-//Create check for win function
+//NOTE TO SELF - Create check for win function and insert into the above P1 and P2 functions to only run after counter >= 6
+var checkForWin = function() {
+    //check rows and columns
+    C1Array = document.querySelectorAll(".C1");
+        if (C1Array[0].innerHTML === C1Array[1].innerHTML && C1Array[1].innerHTML === C1Array[2].innerHTML) {
+            console.log("C1 matches!")//change to something more obvs later.
+        } else {
+            console.log("C1 is not a win.")//i think this can just be empty, must test
+        }
+    C2Array = document.querySelectorAll(".C2");
+        if (C2Array[0].innerHTML === C2Array[1].innerHTML && C2Array[1].innerHTML === C2Array[2].innerHTML) {
+            console.log("C2 matches!")//change to something more obvs later.
+        } else {
+            console.log("C2 is not a win.")//i think this can just be empty, must test
+        }
+    C3Array = document.querySelectorAll(".C3");
+        if (C3Array[0].innerHTML === C3Array[1].innerHTML && C3Array[1].innerHTML === C3Array[2].innerHTML) {
+            console.log("C3 matches!")//change to something more obvs later.
+        } else {
+            console.log("C3 is not a win.")//i think this can just be empty, must test
+        }
+    R1Array = document.querySelectorAll(".R1");
+        if (R1Array[0].innerHTML === R1Array[1].innerHTML && R1Array[1].innerHTML === R1Array[2].innerHTML) {
+            console.log("R1 matches!")//change to something more obvs later.
+        } else {
+            console.log("R1 is not a win.")//i think this can just be empty, must test
+        }
+    R2Array = document.querySelectorAll(".R2");
+        if (R2Array[0].innerHTML === R2Array[1].innerHTML && R2Array[1].innerHTML === R2Array[2].innerHTML) {
+            console.log("R2 matches!")//change to something more obvs later.
+        } else {
+            console.log("R2 is not a win.")//i think this can just be empty, must test
+        }
+    R3Array = document.querySelectorAll(".R3");
+        if (R3Array[0].innerHTML === R3Array[1].innerHTML && R3Array[1].innerHTML === R3Array[2].innerHTML) {
+            console.log("R3 matches!")//change to something more obvs later.
+        } else {
+            console.log("R3 is not a win.")//i think this can just be empty, must test
+        }
+};
+
+var matchCount = 0;
+
+for (let i = 1; i < C1Array.length; i++) {
+    if (C1Array[0].innerHTML === C1Array[i].innerHTML) {
+        matchCount += 1;
+            if matchCount === 2 {
+                console.log("issa match!")
+            } else {
+                matchCount = 0
+            }
+    }
+};
