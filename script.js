9da2a784ec26fdac20428bf4139a4c7a2620e3b9
kinskin <md.asshikin@gmail.com>
2019-07-02 18:47:48 +0800

    added eventlistener to all cells in game board

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 var startGame = function(event){
     console.log("Game Board created");
 }
+var k = 0;
 
+var clickFunction = function(event){
+    console.log("This function happened");
+}
+
+//create table
 var tableCreate = function(){
     var table = document.createElement("table");
-
     for (let i = 0; i < 3; i++){
         var tr = table.insertRow();
         for (let j = 0; j < 3; j++){
@@ -13,15 +18,21 @@ var tableCreate = function(){
             }
             else{
                 var td = tr.insertCell();
-                td.setAttribute("id", j)
+                td.classList.add("box");
                 td.style.width = "200px";
                 td.style.height= "200px";
                 td.style.border = "1px solid black";
+                k++;
+                td.setAttribute("id", k);
+                td.addEventListener("click", clickFunction);
                 tr.appendChild(td);
+
             }
             table.appendChild(tr)
         }
     document.body.appendChild(table);
-    }
+}
 }
 tableCreate();
+
+//create the game
