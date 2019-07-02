1b8fb4290be15be12cfbde5a6d3f456b248bc696
gitwenlei <ngwenlei@gmail.com>
2019-07-02 17:29:00 +0800

    Added base game logic in comments

---
+++ script.js
@@ -0,0 +1,8 @@
+// Step-1. start the game
+// Step-2. ame-squares appear
+// Step-3. user clicks a box (x)
+// Step-4. user clicks another box (o)
+// Step-5. go back to step 3.
+
+
+// user clicks on game-square, text of element changes to X
