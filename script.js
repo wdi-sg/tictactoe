62e1d58f68d6fd61608baf38035f09ed244bd404
Sarah Choo <scchiali@gmail.com>
2019-07-02 21:11:59 +0800

    Prepare for second version DOM manipulation

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
 console.log("Hello");
 
-// var boxFlippedOver = null;
 var marker = ["X", "O"];
+var turnNumber = 0;
 
 var clickBox = document.getElementsByClassName("game-field");
 
+// set marker to divs
 var markedBox = function(event) {
-    console.log("clickedHappened");
-    console.log(event);
-    event.target.innerHTML = "X";
+    // console.log("original turn number", turnNumber);
+    //if turnNumber is even, playerOne turn
+    if (turnNumber % 2 === 0) {
+        //since it is playerOne turn, marker is X
+        event.target.innerHTML = marker[0];
+    }
+    //else is playerTwo turn
+    else {
+        //since it is playerTwo turn, marker is O
+        event.target.innerHTML = marker[1];
+    };
+
+    //after setting the marker, update turnNumber
+    turnNumber++;
+    console.log("new turn number", turnNumber);
 };
-        // var boxImage = event.target;
-        // boxFlippedOver = boxImage;
 
+    // Add event listener to the boxes to track players click
+var addListener = function(){
+    for (var i = 0; i < clickBox.length; i++) {
+        clickBox[i].addEventListener('click', markedBox);
+    }
+};
 
-for (var i = 0; i < clickBox.length; i++) {
-    clickBox[i].addEventListener('click', markedBox);
-}
+document.addEventListener('DOMContentLoaded', addListener);
