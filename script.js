8af0620203c80ad1527b28f70e151738faffb768
Yew <limtheanyew@gmail.com>
2019-07-02 21:43:35 +0800

    First Version of tictactoe

---
+++ script.js
@@ -0,0 +1,40 @@
+var board;
+//var playerOne = 'o';
+//var playerTwo = 'x';
+var playerTurn = null;
+//const winPatterns;
+
+var btnReplay = document.getElementById("id-btn-replay");
+const cells = document.querySelectorAll(".class-button");
+
+var startGame = function() {
+  console.log("Game Started");
+  btnReplay.style.display = "none";
+    board = Array.from(Array(9).keys());
+    for(var i = 0; i <cells.length; i++){
+        cells[i].innerText = '';
+        cells[i].style.removeProperty('background-color');
+        //LINE 16 MAY HAVE PROB
+        cells[i].addEventListener('click', turnclick);
+    }
+};
+var turnclick = function(square){
+    //console.log(square.target.id);
+    if(playerTurn == null || playerTurn == 'o'){
+        playerTurn = 'x'
+    }
+    else {
+        playerTurn = 'o'
+    };
+    turn(square.target.id, playerTurn);
+};
+
+var turn = function(cellId, player) {
+    board[cellId] = player;
+    document.getElementById(cellId).innerHTML = player;
+}
+
+
+//Run StartGame once when program starts
+startGame();
+
