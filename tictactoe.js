62c174adf292a65808eff1d9b86932a0cdfcd970
yoda14 <choobthenoob@hotmail.com>
2019-07-03 00:33:43 +0800

    Aliens vs pineapples!

---
+++ tictactoe.js
@@ -0,0 +1,94 @@
+var saySomething = function(){
+  console.log("YES!");
+};
+
+var element = document.getElementById("helloWord");
+
+element.addEventListener("click", function(){
+document.getElementById("search").value = "Hello World";
+});
+
+// element.addEventListener("mouseover",saySomething);
+
+// var other = document.getElementById("search");
+// other.addEventListener("keypress", saySomething);
+// other.addEventListener("click", function(){
+// document.getElementById("search").value = "Hello World"});
+
+var vegetable1 = document.getElementById("one");
+vegetable1.addEventListener("click", function(){
+    document.getElementById("search").value =
+    vegetable1.innerText;
+});
+
+var boxclick1 = document.getElementById("box1");
+    boxclick1.addEventListener("click", function(){
+    document.getElementById("box1").innerText =
+    "👽";});
+    boxclick1.addEventListener("dblclick", function(){
+    document.getElementById("box1").innerText =
+    "🍍";});
+
+var boxclick2 = document.getElementById("box2");
+    boxclick2.addEventListener("click", function(){
+    document.getElementById("box2").innerText =
+    "👽";});
+    boxclick2.addEventListener("dblclick", function(){
+    document.getElementById("box2").innerText =
+    "🍍";});
+
+var boxclick3 = document.getElementById("box3");
+    boxclick3.addEventListener("click", function(){
+    document.getElementById("box3").innerText =
+    "👽";});
+    boxclick3.addEventListener("dblclick", function(){
+    document.getElementById("box3").innerText =
+    "🍍";});
+
+var boxclick4 = document.getElementById("box4");
+    boxclick4.addEventListener("click", function(){
+    document.getElementById("box4").innerText =
+    "👽";});
+    boxclick4.addEventListener("dblclick", function(){
+    document.getElementById("box4").innerText =
+    "🍍";});
+
+var boxclick5 = document.getElementById("box5");
+    boxclick5.addEventListener("click", function(){
+    document.getElementById("box5").innerText =
+    "👽";});
+    boxclick5.addEventListener("dblclick", function(){
+    document.getElementById("box5").innerText =
+    "🍍";});
+
+var boxclick6 = document.getElementById("box6");
+    boxclick6.addEventListener("click", function(){
+    document.getElementById("box6").innerText =
+    "👽";});
+    boxclick6.addEventListener("dblclick", function(){
+    document.getElementById("box6").innerText =
+    "🍍";});
+
+var boxclick7 = document.getElementById("box7");
+    boxclick7.addEventListener("click", function(){
+    document.getElementById("box7").innerText =
+    "👽";});
+    boxclick7.addEventListener("dblclick", function(){
+    document.getElementById("box7").innerText =
+    "🍍";});
+
+var boxclick8 = document.getElementById("box8");
+    boxclick8.addEventListener("click", function(){
+    document.getElementById("box8").innerText =
+    "👽";});
+    boxclick8.addEventListener("dblclick", function(){
+    document.getElementById("box8").innerText =
+    "🍍";});
+
+var boxclick9 = document.getElementById("box9");
+    boxclick9.addEventListener("click", function(){
+    document.getElementById("box9").innerText =
+    "👽";});
+    boxclick9.addEventListener("dblclick", function(){
+    document.getElementById("box9").innerText =
+    "🍍";});
