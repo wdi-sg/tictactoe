47db19f3da0df93ec0fdebe64e8b4bda389ab934
Axel <email@axeljacob.com>
2019-07-03 23:25:49 +0800

    manage to get the boxes to somewhat interct.

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log('online')
+//create restart button
+var restart = document.querySelector("button");
+
+//grab all the boxes
+var box = document.querySelectorAll("td");
+
+//clear all the boxes. Create a function using For loop
+function clearBoard(){
+   for (var i = 0; i < box.length; i++){
+        console.log(box[i]);
+    box[i].innerHTML = "";
+   }
+}
+
+restart.addEventListener('Click', clearBoard);
+
+//check box marker
+function changeMarker(){
+    if(this.textContent = ''){
+        this.innerHTML === 'X';
+    } else if (this.textContent === 'X') {
+        this.textContent = 'O';
+    } else {
+        this.textContent = '';
+    }
+}
+
+for (var i = 0; i < box.length; i++) {
+    box[i].addEventListener('click', changeMarker)
+}
