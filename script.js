1878a3fcc276bb9f7e2b2afa68580a4ed7c3d2d9
Yi Xin Ang <angyixin@gmail.com>
2019-07-03 14:35:38 +0800

    able to dynamically generate board

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 console.log('linked');
 
-var userATurn = true;
 var displayTurn = document.getElementById('display-turn');
 var gameboardUI = document.getElementById('gameboard');
+var container = document.querySelector('.container');
+
+//game defaults
+var userATurn = true;
 var numTiles = 9;
 var userGame = [];
 var gameboard = [];
 var boardSize = 3;
+var game = [];
+
+// display Player turn
 
 var indicatePlayerTurn = function(){
   if (userATurn === true){
@@ -16,44 +22,43 @@ var indicatePlayerTurn = function(){
   }
 }
 
-var newCreateBoard = function(){
-  for (let a = 0; a < boardSize; a++) {
-        let row = [];
+// dynamically create board based on boardSize
 
-        for (let b = 0; b < boardSize; b++) {
+var createBoard = function(){
+  var board = document.createElement('div');
+  board.setAttribute('id', 'gameboard');
+  container.appendChild(board);
+  for (var i = 0; i < boardSize; i++) {
+        var row = [];
+
+        for (var j = 0; j < boardSize; j++) {
             row.push("");
+            var box = document.createElement('div');
+            box.setAttribute('class', "box");
+            box.setAttribute('x',[j]);
+            box.setAttribute('y',[i]);
+            box.style.width = 100/boardSize + "%";
+            board.appendChild(box);
+            box.addEventListener('click', function(){
+              drawSymbol(event);
+              console.log("in create board function");
+              console.log(event);
+            });
         }
-        gameboard.push(row);
+        game.push(row);
       }
-  console.log(gameboard);
-  return gameboard;
+  console.log(game);
+  return game;
 }
 
-newCreateBoard();
+createBoard();
 
 
 var sayHi = function(){
   console.log('hi');
 }
 
-var createBoardUI = function(){
-  for (var i =0; i < numTiles; i++){
-    var box = document.createElement('div');
-    box.setAttribute("class", "box");
-    box.setAttribute("id", "box-" + (i+1));
-    box.innerHTML = "";
-    gameboardUI.appendChild(box);
-    box.addEventListener('click', function(){
-      drawSymbol(event);
-      console.log("in create board function");
-      console.log(event);
-    });
-  }
-  console.log('end');
-}
-
 indicatePlayerTurn();
-createBoardUI();
 
 var drawSymbol = function(event){
   console.log("in drawSymbol function");
