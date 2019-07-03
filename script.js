2dd2421a4702efa7c1a15b891bfef848ec54051e
shirleytwl <shirtanwl@gmail.com>
2019-07-03 17:58:21 +0800

    added restart game

--- script.js
+++ script.js
@@ -8,12 +8,52 @@ window.onload = function() {
     var boardTracker = [
       [ 0, 0, 0 ],
       [ 0, 0, 0 ],
-      [ 0, 0, 0 ]
+        [ 0, 0, 0 ]
     ]; //1 is ✕ player, -1 is ◯ player
+    var numOfSquares = boardTracker.length;
     var winner = null;
     var loser = null;
     var startButton = document.getElementById("start-button");
     var wrapper = document.getElementById("wrapper");
+
+    var createBoard = function(event) {
+        if (document.getElementById("board") != null){
+            wrapper.removeChild(document.getElementById("board"));
+        }
+        startButton.style.display = "none";
+        displayMsg.innerText = `${xTurn} player's turn`;
+        wrapper.appendChild(displayMsg);
+        var board = document.createElement("div");
+        board.setAttribute("id","board");
+
+        var gameRow = document.createElement("div");
+        gameRow.setAttribute("class","game-row");
+
+        var gameSquare = document.createElement("span");
+        gameSquare.setAttribute("class","game-square");
+
+        let row = 0;
+        let col = 0;
+        for (var i=0;i<3;i++){
+            let clonedSquare = gameSquare.cloneNode(true);
+            clonedSquare.setAttribute("data-col",col);
+            gameRow.appendChild(clonedSquare);
+            col++;
+        }
+        for (var i=0;i<3;i++){
+            let clonedRow = gameRow.cloneNode(true);
+            clonedRow.setAttribute("data-row",row);
+            board.appendChild(clonedRow);
+            row++
+        }
+        wrapper.appendChild(board);
+        let squares = document.querySelectorAll(".game-square");
+        for (var i=0;i<squares.length;i++){
+            squares[i].addEventListener("click",fillSquare);
+        }
+        gameStarted = true;
+    }
+    startButton.addEventListener("click",createBoard);
 
     var fillSquare = function(event) {
         if (!winner){
@@ -32,7 +72,17 @@ window.onload = function() {
             }
             if (numOfTurns>2) {
                 if (checkGame()) {
+                    startButton.innerText = "Restart Game";
+                    startButton.style.display = "inline-block";
                     displayMsg.innerText = `${winner} player wins!\n${loser} player have to treat ${winner} player a drink.`;
+                    winner = null;
+                    loser = null;
+                    boardTracker = [
+                      [ 0, 0, 0 ],
+                      [ 0, 0, 0 ],
+                        [ 0, 0, 0 ]
+                    ];
+                    numOfTurns = 0;
                 }
                 else if (numOfTurns>8){
                     displayMsg.innerText = "Game Draw!";
@@ -44,11 +94,9 @@ window.onload = function() {
         let row = rowEl.dataset.row;
         let col = squareEl.dataset.col;
         boardTracker[row][col] = value;
-        console.log(boardTracker);
     }
     var checkGame = function() {
         let totalValue = 0 //if total is -3 or 3, means a player won
-        let numOfSquares = boardTracker.length;
         //check horizontally
         //loop through rows
         for (var i=0;i<numOfSquares;i++){
@@ -56,13 +104,13 @@ window.onload = function() {
             for (var j=0;j<boardTracker[i].length;j++){
                 totalValue = totalValue + boardTracker[i][j];
                 if (totalValue === 3) {
-                    winner = "✕";
-                    loser = "◯";
+                    winner = xTurn;
+                    loser = oTurn;
                     return true;
                 }
                 else if (totalValue === -3){
-                    winner = "◯";
-                    loser = "✕";
+                    winner = oTurn;
+                    loser = xTurn;
                     return true;
                 }
             }
@@ -76,13 +124,13 @@ window.onload = function() {
             for (var j=0;j<boardTracker[i].length;j++){
                 totalValue = totalValue + boardTracker[j][currentCol];
                 if (totalValue === 3) {
-                    winner = "x";
-                    loser = "◯";
+                    winner = xTurn;
+                    loser = oTurn;
                     return true;
                 }
                 else if (totalValue === -3){
-                    winner = "o"
-                    loser = "✕";
+                    winner = oTurn;
+                    loser = xTurn;
                     return true;
                 }
             }
@@ -94,13 +142,13 @@ window.onload = function() {
             totalValue = totalValue + boardTracker[i][i];
             //console.log(totalValue);
             if (totalValue === 3) {
-                winner = "x";
-                loser = "◯";
+                winner = xTurn;
+                loser = oTurn;
                 return true;
             }
             else if (totalValue === -3){
-                winner = "o"
-                loser = "✕";
+                winner = oTurn;
+                loser = xTurn;
                 return true;
             }
             totalValue = 0;
@@ -109,55 +157,16 @@ window.onload = function() {
         for (var i=0;i<numOfSquares;i++){
             totalValue = totalValue + boardTracker[i][numOfSquares-(i+1)];
             if (totalValue === 3) {
-                winner = "x";
-                loser = "◯";
+                winner = xTurn;
+                loser = oTurn;
                 return true;
             }
             else if (totalValue === -3){
-                winner = "o"
-                loser = "✕";
+                winner = oTurn;
+                loser = xTurn;
                 return true;
             }
         }
-
         return false;
     }
-    var createBoard = function(event) {
-        if (!gameStarted) {
-            displayMsg.innerText = `${xTurn} player's turn`;
-            wrapper.appendChild(displayMsg);
-
-            var board = document.createElement("div");
-            board.setAttribute("id","board");
-
-            var gameRow = document.createElement("div");
-            gameRow.setAttribute("class","game-row");
-
-            var gameSquare = document.createElement("span");
-            gameSquare.setAttribute("class","game-square");
-
-            let row = 0;
-            let col = 0;
-            for (var i=0;i<3;i++){
-                let clonedSquare = gameSquare.cloneNode(true);
-                clonedSquare.setAttribute("data-col",col);
-                //clonedSquare.addEventListener("onclick",function(){console.log("YTA")});
-                gameRow.appendChild(clonedSquare);
-                col++;
-            }
-            for (var i=0;i<3;i++){
-                let clonedRow = gameRow.cloneNode(true);
-                clonedRow.setAttribute("data-row",row);
-                board.appendChild(clonedRow);
-                row++
-            }
-            wrapper.appendChild(board);
-            let squares = document.querySelectorAll(".game-square");
-            for (var i=0;i<squares.length;i++){
-                squares[i].addEventListener("click",fillSquare);
-            }
-            gameStarted = true;
-        }
-    }
-    startButton.addEventListener("click",createBoard);
 }
