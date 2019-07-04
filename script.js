23beba7384e0f0c2fd50fbceceee224c3f27e14d
Sarah Choo <scchiali@gmail.com>
2019-07-04 08:58:55 +0800

    Part 1

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ console.log("Hello");
 var marker = ["X", "O"];
 var turnNumber = 0;
 
+//get element by class name always return an array of elements
 var clickBox = document.getElementsByClassName("game-field");
 
 // set marker to divs
@@ -24,11 +25,29 @@ var markedBox = function(event) {
     console.log("new turn number", turnNumber);
 };
 
-    // Add event listener to the boxes to track players click
+// Add event listener to the boxes to track players click
 var addListener = function(){
     for (var i = 0; i < clickBox.length; i++) {
+        //it is an array
         clickBox[i].addEventListener('click', markedBox);
     }
 };
 
-document.addEventListener('DOMContentLoaded', addListener);
+var initializeGame = function() {
+    var gameBoard = document.createElement("div");
+    //create an ID to element. For ID only
+    gameBoard.setAttribute("id", "board");
+    //create 9 boxes
+    for (var i = 0; i < 9; i++) {
+        //for each loop, it creates one box
+        var box = document.createElement("div");
+        box.classList.add("game-field");
+        // box.addEventListener('click', markedBox);
+        gameBoard.appendChild(box);
+    };
+
+    document.body.appendChild(gameBoard);
+    addListener(); //or use box.addEventListener('click', markedBox);
+}
+
+document.addEventListener('DOMContentLoaded', initializeGame);
