6e516b477696ad24e709394ab5805cf97674f547
“woofborn” <wolf.born@gmail.com>
2019-09-24 18:30:06 +0800

    Right before creating nested for loop. Hope this works.

--- script.js
+++ script.js
@@ -7,29 +7,59 @@ var board = [
     [null, null, null]
 ]
 
+var boardSquares = 9;
+var counter = 0;
+
+function createBoard(){
+    var body = document.querySelector("body");
+    var divContainer = document.createElement("div");
+    divContainer.setAttribute("id", "container");
+    body.appendChild(divContainer);
+    //add div class boxes
+    for (let i =0; i<boardSquares; i++){
+        var box = document.createElement("div");
+        box.setAttribute("class", "box");
+        box.setAttribute("id", `box${[i]}`);
+        divContainer.appendChild(box);
+    }
+}
+createBoard();
+
+
 var emptyBox = document.querySelectorAll(".box");
-var counter = 0
 
-
-
-
+// add X or O to board on click
 function addItem(){
 
     counter = counter+1;
     console.log( counter );
 
-    var clickedBox = event.target
-
+    var clickedBox = event.target;
     console.log("Click worked");
 
     if (counter === 0 || counter%2 === 0){
         clickedBox.innerText = "X"
     } else {
        clickedBox.innerText = "O"
+    };
+
+    var firstBox= document.getElementById("box0")
+
+
+    if (firstBox.innerText === "X"){
+        board[0][0] = "X";
+    } else if (firstBox.innerText === "O"){
+        board[0][0] = "0"
+console.log(board)
     }
+
+
+//If you click on first box, board [0][0]=filled
+
+
 }
 
-
-for(var i = 0; i < emptyBox.length; i++){
+// create event listener for each box
+for(let i = 0; i < emptyBox.length; i++){
     emptyBox[i].addEventListener("click", addItem);
 }
