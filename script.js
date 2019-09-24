a93a982f9b80afa709db9aa644f6c70e721f25dd
lhjd <hongjinlau@gmail.com>
2019-09-25 00:01:14 +0800

    Add functions to get columns, rows and diagonals

--- script.js
+++ script.js
@@ -6,23 +6,28 @@ var board = [[1,2,3],
              [4,5,6],
              [7,8,9]];
 
-var rows = [];
-var columns = [];
-var diagonals = [[],[]];
+var rows;
+var columns;
+var diagonals;
+var sets;
 
-var getRows = function() {
+var getRows = function(board) {
+    var rows = [];
     board.forEach(row => rows.push(row));
+    return rows;
 };
 
-var getColumns = function() {
+rows = getRows(board);
+console.log("rows: ", rows);
+
+var getColumns = function(board) {
+    var columns = [];
     // add empty arrays to columns
     for(var i = 0; i < board.length ; i++) {
         columns.push([]);
     }
 
-    console.log(columns);
-
-    // push squares to columns
+    // push squares to empty arrays
     for(var i=0; i< board.length; i++) {
         var row = board[i];
         for(var j=0; j<row.length; j++) {
@@ -30,7 +35,55 @@ var getColumns = function() {
             columns[j].push(square);
         }
     }
+
+    return columns;
+};
+
+columns = getColumns(board);
+console.log("columns: ", columns);
+
+var convertBoardToList = function(board) {
+    var list = [];
+    board.forEach(row => row.forEach(square => list.push(square)));
+    return list;
+};
+
+var getMirroredBoard = function(board) {
+    var mirroredBoard = [];
+    board.forEach(row => mirroredBoard.push( row.slice().reverse() ));
+    return mirroredBoard;
+};
+
+var getDiagonal = function(list, board) {
+    var diagonal = [];
+    for (var i = 0; i < list.length; i += board.length + 1) {
+        diagonal.push(list[i]);
+    }
+    return diagonal;
+};
+
+var getDiagonals = function(board) {
+    var diagonals = [];
+
+    var mirroredBoard = getMirroredBoard(board);
+
+    var list = convertBoardToList(board);
+    var mirroredList = convertBoardToList(mirroredBoard);
+
+    var diagonal = getDiagonal(list,board);
+    var mirroredDiagonal = getDiagonal(mirroredList, mirroredBoard);
+
+    diagonals.push(diagonal);
+    diagonals.push(mirroredDiagonal);
+
+    return diagonals;
 }
+
+diagonals = getDiagonals(board);
+console.log("diagonals: ", diagonals);
+
+sets = rows.concat(columns, diagonals);
+console.log("sets: ", sets);
 
 // add marker to board
 // update board with input
