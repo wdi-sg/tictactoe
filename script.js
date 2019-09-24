1de47c85759a2ef10b7a9fec73fc22ae6b2c4909
dannyozh <danielongzhihao@gmail.com>
2019-09-24 17:44:51 +0800

    able to click on each box and return an 'X'.

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 console.log("hello");
+
+var allSquares = document.querySelectorAll(".game-square");
+
+
+var doSomething = function (event) {
+    console.log("something is going on");
+    event.target.innerText = "X";
+}
+
+
+var attachListenersToBoard = function () {
+    console.log("looping through");
+    for (i = 0; i < allSquares.length; i++) {
+        var bigSquare = allSquares[i];
+        bigSquare.addEventListener('click', doSomething);
+    }
+};
+
+inputGameTurn();
