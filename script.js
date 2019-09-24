c841b7a4a6f92a70a6ff413d1eda4fcfdb2e9c7e
lhjd <hongjinlau@gmail.com>
2019-09-25 01:46:03 +0800

    flexible board size

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
-// stores X or O
 var marker = null;
 var markerType = {
     A: "O",
     B: "X",
 }
-//
-var board = [[null,null,null],
-             [null,null,null],
-             [null,null,null]];
+var board = [];
+var boardSize = 3;
+var rows = [];
+var columns = [];
+var diagonals = [];
+var sets = [];
 
-var boardSize = null;
 
-var rows;
-var columns;
-var diagonals;
-var sets;
+var createBoardObj = function(boardSize) {
+    var newboard = [];
+
+    for(var i = 0; i < boardSize; i++) {
+        var row = [];
+        for(var j = 0; j < boardSize; j++){
+            row.push(null);
+        }
+        newboard.push(row);
+    }
+    return newboard;
+};
 
 var getRows = function(board) {
     var rows = [];
@@ -22,20 +30,17 @@ var getRows = function(board) {
     return rows;
 };
 
-rows = getRows(board);
-console.log("rows: ", rows);
-
 var getColumns = function(board) {
     var columns = [];
     // add empty arrays to columns
-    for(var i = 0; i < board.length ; i++) {
+    for(var i = 0; i < boardSize ; i++) {
         columns.push([]);
     }
 
     // push squares to empty arrays
-    for(var i=0; i< board.length; i++) {
+    for(var i=0; i < boardSize; i++) {
         var row = board[i];
-        for(var j=0; j<row.length; j++) {
+        for(var j=0; j< row.length; j++) {
             var square = row[j];
             columns[j].push(square);
         }
@@ -43,9 +48,6 @@ var getColumns = function(board) {
 
     return columns;
 };
-
-columns = getColumns(board);
-console.log("columns: ", columns);
 
 var convertBoardToList = function(board) {
     var list = [];
@@ -61,7 +63,7 @@ var getMirroredBoard = function(board) {
 
 var getDiagonal = function(list, board) {
     var diagonal = [];
-    for (var i = 0; i < list.length; i += board.length + 1) {
+    for (var i = 0; i < list.length; i += boardSize + 1) {
         diagonal.push(list[i]);
     }
     return diagonal;
@@ -84,10 +86,6 @@ var getDiagonals = function(board) {
     return diagonals;
 }
 
-diagonals = getDiagonals(board);
-console.log("diagonals: ", diagonals);
-
-
 var getSets = function(board) {
     var rows = getRows(board);
     var columns = getColumns(board);
@@ -98,46 +96,38 @@ var getSets = function(board) {
     return sets;
 }
 
-
-sets = getSets(rows, columns, diagonals);
-console.log("sets: ", sets);
-
-
 var checkWinner = function(sets, markerType, board) {
     var markerA = markerType.A;
     var markerB = markerType.B;
 
     var setA = sets.map(set => set.filter(square => square === markerA).length);
     var setB = sets.map(set => set.filter(square => square === markerB).length);
-    // console.log(setA);
-    // console.log(setB);
-    // debugger;
-    if (setA.includes(board.length)) {
+
+    if (setA.includes(boardSize)) {
         return markerA;
-    } else if (setB.includes(board.length)) {
+    } else if (setB.includes(boardSize)) {
         return markerB;
     } else { return "no winner"; }
 };
 
-// var winner = checkWinner(sets, markerType, board);
-// console.log(winner);
-
-// add marker to board
-// update board with input
 var addMarker = function(event) {
-    // debugger;
+
     if (marker === null || marker === markerType.A) {
         marker = markerType.B;
     } else {
         marker = markerType.A;
     }
+
     event.target.innerText = marker;
 
     var col = parseInt(event.target.id);
     var row = parseInt(event.target.parentElement.id);
 
     board[row][col] = marker;
-    // console.log(board);
+
+    var rows = getRows(board);
+    var columns = getColumns(board);
+    var diagonals = getDiagonals(board);
     var sets = getSets(rows, columns, diagonals);
 
     var winner = checkWinner(sets, markerType, board);
@@ -150,33 +140,52 @@ var addMarker = function(event) {
 
 // create board
 var createBoard = function() {
-    var board = document.createElement("div");
-    board.setAttribute("id", "board");
+    var boardDiv = document.createElement("div");
+    boardDiv.setAttribute("id", "board");
 
-    // append 3 rows to board
-    for (var i = 0; i < 3; i++) {
+    board = createBoardObj(boardSize);
+
+    // append x rows to board
+    for (var i = 0; i < boardSize; i++) {
         var gameRow = document.createElement("div");
         gameRow.classList.add("game-row");
         gameRow.setAttribute("id", i);
         // append 3 squares to row
-        for (var j = 0; j < 3; j++) {
+        for (var j = 0; j < boardSize; j++) {
             var gameSquare = document.createElement("span");
             gameSquare.classList.add("game-square");
             gameSquare.setAttribute("id", j);
             gameSquare.addEventListener("click", addMarker, { once: true });
             gameRow.appendChild(gameSquare);
         }
-        board.appendChild(gameRow);
+        boardDiv.appendChild(gameRow);
     }
 
     // append board to game interface
     var gameInterface = document.querySelector(".game-interface");
-    gameInterface.appendChild(board);
+    gameInterface.appendChild(boardDiv);
 
     // remove the start button
     var startButton = document.querySelector(".start-button");
     startButton.remove();
+
+    var boardSizeInput = document.querySelector("#board-size");
+    boardSizeInput.remove();
+
+    var boardSizeLabel = document.querySelector(".board-size");
+    boardSizeLabel.remove();
 };
+
+var getBoardSize = function(event) {
+    boardSize = parseInt(event.target.value);
+    console.log("board size: ", boardSize);
+    board = createBoardObj(boardSize);
+    console.log("new board", board);
+    createBoard();
+};
+
+var boardSizeInput = document.querySelector("#board-size");
+boardSizeInput.addEventListener("change", getBoardSize);
 
 var startButton = document.querySelector(".start-button");
 startButton.addEventListener("click", createBoard);
