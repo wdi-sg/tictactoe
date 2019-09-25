0ca2e01ae0c80f850db97088f7ef662e89522470
syahirah <syahirah.suradi@gmail.com>
2019-09-26 00:23:31 +0800

    define win types, added restart button

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
-
+var gameBoard;
+var player1;
+var player2;
+const winTypes = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [0, 4, 8]
+]
 //retrieve all spans from HTML and assign it variable square; the large game board
 var square = document.querySelectorAll('.game-square');
-console.log(square);
+//call startGame function
+startGame();
+
+function startGame(){
+    gameBoard = Array.from(Array(9).keys());
+    for (var i = 0; i < square.length; i++){
+        square[i].innerText = "";
+        square[i].addEventListener("click", checked, false);
+    }
+}
 //tracks no.of player clicks
 var counter = 0;
 
 //Assign click event target to individual spans
 function checked(event){
-    console.log(event.target);
     var squareIndividual = event.target;
     //checks counter for even or odd clicks
-    console.log("before conditional" , counter);
+        //console.log("before conditional" , counter);
     //if click is even, insert text to span as 'X'
     if (counter % 2 === 0) {
         squareIndividual.textContent = "X";
@@ -18,15 +38,17 @@ function checked(event){
     }   else if (counter % 2 !== 0) {
         squareIndividual.textContent = "O";
     }
+    // document.getElementById(squareIndividual).removeEventListener("click", checked);
     counter++;
-    console.log("after conditional" , counter);
+    console.log(squareIndividual.id);
+        //console.log("after conditional" , counter);
 };
-
 //loops through the whole var square array and assign event lister & 'checked' function to it
-var i = 0;
+/*var i = 0;
 while (i<square.length){
     var board = square[i];
     console.log(board);
     board.addEventListener('click', checked);
     i++;
-}
+}*/
+//function to check whether var squareIndividual has been clicked. If yes, remove event listener
