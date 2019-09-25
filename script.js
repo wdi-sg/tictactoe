c17d38ec5cf4deecfa0d3b808386728c19ab4a0e
jasminesis <jasmine-aips@hotmail.com>
2019-09-25 21:23:09 +0800

    boardappears.js contains all the DOM creation for board but not working together with existing code for checking winning combinations

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
+var ticboard = document.querySelector('div');
+
 var p1board = [null, null, null, null, null, null, null, null, null];
 var p2board = [null, null, null, null, null, null, null, null, null];
 
-var a = document.querySelectorAll('span')[0]
-var b = document.querySelectorAll('span')[1]
-var c = document.querySelectorAll('span')[2]
-var d = document.querySelectorAll('span')[3]
-var e = document.querySelectorAll('span')[4]
-var f = document.querySelectorAll('span')[5]
-var g = document.querySelectorAll('span')[6]
-var h = document.querySelectorAll('span')[7]
-var i = document.querySelectorAll('span')[8]
+var a = document.querySelectorAll('span')[0];
+var b = document.querySelectorAll('span')[1];
+var c = document.querySelectorAll('span')[2];
+var d = document.querySelectorAll('span')[3];
+var e = document.querySelectorAll('span')[4];
+var f = document.querySelectorAll('span')[5];
+var g = document.querySelectorAll('span')[6];
+var h = document.querySelectorAll('span')[7];
+var i = document.querySelectorAll('span')[8];
 
 tictactoe = {
     a: 0,
@@ -27,12 +29,19 @@ var playerTurn = 0;
 i = 0;
 
 
+
+var gameSquares = document.getElementsByClassName('game-square');
+//changes colour for the square if clicked
+for (i = 0; i < gameSquares.length; i++) {
+    gameSquares[i].addEventListener('click', turnSquare)
+}
+
 function turnSquare() {
     var clicked = event.target;
     if (playerTurn % 2 === 0 && playerTurn < 9) {
         playerTurn++;
-        console.log(playerTurn);
-        clicked.style.backgroundColor = 'purple';
+        console.log(`Player turn: ${playerTurn}`);
+        clicked.style.backgroundColor = '#30c6d1';
             if (clicked === a) {
                 p1board[tictactoe['a']] = 'x';
             } else if (clicked === b) {
@@ -56,7 +65,7 @@ function turnSquare() {
     } else if (playerTurn % 2 === 1 && playerTurn < 9) {
         playerTurn++;
         console.log(playerTurn);
-        clicked.style.backgroundColor = 'blue';
+        clicked.style.backgroundColor = '#991d44';
             if (clicked === a) {
                 p2board[tictactoe['a']] = 'x';
             } else if (clicked === b) {
@@ -82,11 +91,6 @@ function turnSquare() {
     }
 }
 
-var gameSquare = document.getElementsByClassName('game-square');
-
-for (i = 0; i < gameSquare.length; i++) {
-    gameSquare[i].addEventListener('click', turnSquare)
-}
 
 var player;
 
