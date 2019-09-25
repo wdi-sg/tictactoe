b833d69c047275f9e1eb22d907fdbeb1acd6e7cb
dannyozh <danielongzhihao@gmail.com>
2019-09-25 21:23:15 +0800

    cleaned up code. added in comments to understand better.

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 console.log("hello");
 
+//create game counter and 'array' representation of board
+
+var gameStage = 0;
 var board = [
     [null, null, null],
     [null, null, null],
     [null, null, null]
 ];
 
-//put array of squares into variable
+//targeted all squares and stored into variable
 
 var allSquares = document.querySelectorAll(".game-square");
 
@@ -20,13 +23,11 @@ var attachListenersToBoard = function () {
     }
 };
 
-//create game counter global variable
 
-var gameStage = 0;
+//created what will happen after event listener is activated: use  modulus to choose between odd and even.
 
-//created what will happen after event listener is activated
-//if you have two outcomes – here it's "x" and "o", you don't have to blindly code out everything, simply use the modulus to choose between odd and even.
-
+//*note* By assigning id to each square, you can get them "targeted" with each click.
+// continued..we are essentially telling JS where each click takes place.
 
 var doSomething = function (event) {
     console.log(gameStage);
@@ -36,6 +37,7 @@ var doSomething = function (event) {
         if (gameStage%2 === 0 && gameStage < 9) {
             event.target.innerText = "X";
                 board[coordinates[0]][coordinates[1]] = "X";
+//equivalent to board[representation of row][representation of cell]
         } else if (gameStage%2 === 1 && gameStage < 9) {
             event.target.innerText = "O";
                 board[coordinates[0]][coordinates[1]] = "O";
@@ -44,6 +46,8 @@ var doSomething = function (event) {
     console.log(board);
     gameStage ++;
 };
+
+//created winnerCheck function.
 
 
 var winnerCheck = function () {
@@ -72,50 +76,4 @@ var winnerCheck = function () {
     }
 };
 
-
-
-
-
-
-
-// var attachListenersToBoard2 = function () {
-//     console.log("checking for winner");
-//     for (i = 0; i < board.length; i++) {
-//         for (j=0; j < board[i].length; j++) {
-//             var bigBoard = board[i][j];
-//             bigBoard.addEventListener('click', trackSomething);
-//         }
-//     }
-// };
-
-// var trackSomething = function (event) {
-//     console.log("winner function");
-//     if (board[00]
-// }
-
-
-
-// str = "Daniel"
-// str[00], 00, 00
-// arr[1]
-
-//ran attach listener function
-
 attachListenersToBoard();
-
-// var board = {
-//     [null, null, null],
-//     [null, null, null],
-//     [null, null, null]
-// };
-
-// var attachListenersToBoard = function () {
-//     console.log("looping through");
-//     for (i = 0; i < board.length; i++) {
-//         for (i = j; j < board[i].length; j++) {
-//             var playLog = board
-//         }
-//         var bigSquare = allSquares[i];
-//         bigSquare.addEventListener('click', doSomething);
-//     }
-// };
