7c7b4993df255a2f8dd28b1ef13db2f3028a5458
Robert Kolsek <kolsek.income@gmail.com>
2019-12-17 20:59:13 +0800

    Added proper comments for all functions and logic

--- script.js
+++ script.js
@@ -5,18 +5,25 @@ let choices = [
 ]
 
 const clickEvent = function () {
-    
+    //user turn is on
     let userTurn = true
+    //turn number
     let turnNumber = 0
+    //select all squares
     const boxes = document.querySelectorAll(".game-square")
+    //apply for all squares
     boxes.forEach(function (item) {
+        //click event
         item.addEventListener("click", function (el) {
+            //check if less than 9 turns, so nothing happens after
             if (turnNumber < 9) {
                 if (userTurn) {
                     userTurn = !userTurn
                     item.textContent = "X"
+                    //variable for the squares class id number
                     let boxNumber = item.className[item.className.length - 1]
-
+                    //detect if the id is within a certain range
+                    //and assign it to row 0, 1 or 2 with an index of 0,1 or 2
                     if (boxNumber < 3) {
                         choices[0][boxNumber] = "X"
                     } else if (boxNumber < 6){
@@ -30,6 +37,7 @@ const clickEvent = function () {
                 } else {
                     userTurn = !userTurn
                     item.textContent = "O"
+                    //same as above, but for the O player
                     let boxNumber = item.className[item.className.length -1]
                     if (boxNumber < 3) {
                         choices[0][boxNumber] = "O"
@@ -46,6 +54,7 @@ const clickEvent = function () {
             } else {
 
             }
+            //check for win after each input
             checkForWin()
 
         })
@@ -53,6 +62,7 @@ const clickEvent = function () {
 }
 
 const checkForWin = function(){
+    // all possible win combinations
     if (choices[0][0] === "X" && choices[1][0] === "X" && choices[2][0] === "X" || 
     choices[0][0] === "X" && choices[0][1] === "X" && choices[0][2] === "X" ||
      choices[0][2] === "X" && choices[1][2] === "X" && choices[2][2] === "X" ||
@@ -61,7 +71,7 @@ const checkForWin = function(){
         choices[0][2] === "X" && choices[1][1] === "X" && choices[2][0] === "X" ||
          choices[0][1] === "X" && choices[1][1] === "X" && choices[2][1] === "X" ||
          choices[1][0] === "X" && choices[1][1] === "X" && choices[1][2] === "X"){
-             
+        // if won, apply retry
         retryButton()
 
     }
@@ -71,30 +81,34 @@ const createBoard = function () {
     //create board
     const board = document.createElement("div")
     board.className = "board"
-    //create rows
-    const row = document.createElement("div")
-
     //create squares
     const gameSquare = document.createElement("button")
-
+    //give squares a class id and append to board
     for (let i = 0; i < 9; i++) {
         gameSquare.className = `game-square ${[i]}`
         board.appendChild(gameSquare.cloneNode(true))
     }
+    //append board to body
     document.body.appendChild(board)
+    //add clickEvent to squares
     clickEvent()
 }
 
-const startButton = function(){
+//start button
+const startGame = function(){
+    //reset the choices array
     choices = [
         [undefined,undefined,undefined],
         [undefined,undefined,undefined],
         [undefined,undefined,undefined]
     ]
+    //reset body
     document.body.innerHTML = ""
+    //create start button
     const button = document.createElement("button")
     button.innerHTML = "START GAME"
     button.addEventListener("click", function(){
+        //on click let button disappear and create the board with click event
         button.classList.add("d-none")
         createBoard()
     })
@@ -102,19 +116,23 @@ const startButton = function(){
 }
 
 const retryButton = function(){
+    //reset the choices array
     choices = [
         [undefined,undefined,undefined],
         [undefined,undefined,undefined],
         [undefined,undefined,undefined]
     ]
+    //reset body
     document.body.innerHTML = ""
+    //create retry button
     const button = document.createElement("button")
     button.innerHTML = "YOU WON! RETRY?"
     button.addEventListener("click", function(){
+        //on click let button disappear and create the board with click event
         button.classList.add("d-none")
         createBoard()
     })
     document.body.appendChild(button)
 }
 
-startButton()
+startGame()
