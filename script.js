a2e80d6e058b5403b494f3c8c94a22402972661e
upieez <samuelhww@gmail.com>
2019-12-17 20:04:43 +0800

    add function to change text to "x"

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("Tic Tac Toe assignment")
+// console.log("Tic Tac Toe assignment")
+
+// var clickFunction = function(){
+//     console.log("something happened");
+// }
+
+// var clicker = document.querySelectorAll(".game-square");
+
+// clicker.addEventListener("click", clickFunction);
+
+var button = document.querySelectorAll('.game-square');
+
+var clickHandler = function(input){
+  this.innerHTML = "x";
+};
+
+var game = function(){
+    for(var i = 0; i < button.length; i++){
+    button[i].addEventListener('click', clickHandler);
+    }
+};
+
+game();
