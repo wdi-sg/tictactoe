62ee164df55f3687406cfe29774bee3aacb415ae
SamTanFL <samsite_tan@hotmail.com>
2019-12-17 17:18:05 +0800

    added create board & adding attributes

---
+++ script.js
@@ -0,0 +1,54 @@
+console.log("test");
+
+
+var turn = 0;
+var grid = document.querySelector(".grid")
+
+
+
+var createRow = function () {
+    for (i = 0; i < 3; i++) {
+        var row = document.createElement("div");
+        row.setAttribute("class", "row");
+        grid.appendChild(row);
+    }
+}
+
+
+var createColumn = function () {
+    for (i = 0; i < 3; i++) {
+        var box = document.createElement("span");
+        box.setAttribute("class", "box");
+        box.addEventListener("click", settingCheck);
+        document.querySelectorAll(".row")[i].appendChild(box);
+    }
+}
+
+var createBoard = function() {
+    createRow();
+    createColumn();
+    createColumn();
+    createColumn();
+}
+
+var setToX = function() {
+    event.target.setAttribute("class", "x box");
+}
+
+var setToO = function() {
+    event.target.setAttribute("class", "o box");
+}
+
+var settingCheck = function() {
+    if (turn === 0) {
+        setToX()
+        turn++;
+    } else {
+        setToO();
+        turn = 0;
+    }
+}
+
+createBoard();
+/*addSomething();*/
+/*createColumn();*/
