f9ebcbd0b2a0329cfaf14ea50c0088128e8fcfda
SamTanFL <samsite_tan@hotmail.com>
2019-12-18 13:23:07 +0800

    added invisibility to reset button

--- script.js
+++ script.js
@@ -69,72 +69,88 @@ var checkForWin = function () {
                 if (turn === 0){
                 alert("Circle Wins");
                 gameIsOver = true;
+                document.querySelector("#reset").classList.remove("invisible");
                     } else {
                         alert("X Wins");
                         gameIsOver = true;
+                        document.querySelector("#reset").classList.remove("invisible");
                     }
                 break;
             case (box[0].innerText !== "" && box[0].innerText === box[3].innerText && box[0].innerText === box[6].innerText):
                 if (turn === 0){
                 alert("Circle Wins");
                 gameIsOver = true;
+                document.querySelector("#reset").classList.remove("invisible");
                     } else {
                         alert("X Wins");
                         gameIsOver = true;
+                        document.querySelector("#reset").classList.remove("invisible");
                     }
                 break;
             case (box[0].innerText !== "" && box[0].innerText === box[4].innerText && box[0].innerText === box[8].innerText):
                 if (turn === 0){
                 alert("Circle Wins");
                 gameIsOver = true;
+                document.querySelector("#reset").classList.remove("invisible");
                     } else {
                         alert("X Wins");
                         gameIsOver = true;
+                        document.querySelector("#reset").classList.remove("invisible");
                     }
                 break;
             case (box[1].innerText !== "" && box[1].innerText === box[4].innerText && box[1].innerText === box[7].innerText):
                 if (turn === 0){
                 alert("Circle Wins");
                 gameIsOver = true;
+                document.querySelector("#reset").classList.remove("invisible");
                     } else {
                         alert("X Wins");
                         gameIsOver = true;
+                        document.querySelector("#reset").classList.remove("invisible");
                     }
                 break;
             case (box[2].innerText !== "" && box[2].innerText === box[5].innerText && box[2].innerText === box[8].innerText):
                 if (turn === 0){
                 alert("Circle Wins");
                 gameIsOver = true;
+                document.querySelector("#reset").classList.remove("invisible");
                     } else {
                         alert("X Wins");
                         gameIsOver = true;
+                        document.querySelector("#reset").classList.remove("invisible");
                     }
                 break;
             case (box[3].innerText !== "" && box[3].innerText === box[4].innerText && box[3].innerText === box[5].innerText):
                 if (turn === 0){
                 alert("Circle Wins");
                 gameIsOver = true;
+                document.querySelector("#reset").classList.remove("invisible");
                     } else {
                         alert("X Wins");
                         gameIsOver = true;
+                        document.querySelector("#reset").classList.remove("invisible");
                     }
                 break;
             case (box[6].innerText !== "" && box[6].innerText === box[7].innerText && box[6].innerText === box[8].innerText):
                 if (turn === 0){
                 alert("Circle Wins");
                 gameIsOver = true;
+                document.querySelector("#reset").classList.remove("invisible");
                     } else {
                         alert("X Wins");
                         gameIsOver = true;
+                        document.querySelector("#reset").classList.remove("invisible");
                     }
                 break;
             case (box[2].innerText !== "" && box[2].innerText === box[4].innerText && box[2].innerText === box[6].innerText):
                 if (turn === 0){
                 alert("Circle Wins");
                 gameIsOver = true;
+                document.querySelector("#reset").classList.remove("invisible");
                     } else {
                         alert("X Wins");
                         gameIsOver = true;
+                        document.querySelector("#reset").classList.remove("invisible");
                     }
                 break;
             default:
@@ -149,8 +165,7 @@ var resetGame = function () {
     createBoard();
     getBoxIds();
     gameIsOver = false;
+    event.target.classList.add("invisible");
 }
 
 document.querySelector("#reset").addEventListener("click", resetGame);
-createBoard();
-getBoxIds();
