7b7ff7b9950c331037ef096a9507cf82037f9e15
ShaneC <shanes_email_is@yahoo.com>
2019-12-18 18:07:19 +0800

    got tictactoe working.

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 console.log("Hello World");
+
+var player = false
+var emptyBoard = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null]
+];
+var clickHandler = function(event){
+    if(player === false) {
+        this.innerHTML = 'X';
+        player = true
+} else {
+    this.innerHTML = 'O';
+    player = false;
+}
+
+};
+
+// console.log("about to set event handler on element");
+
+var button = document.querySelectorAll('.container-box')
+for (var i=0; i<button.length; i++) {
+    button[i].addEventListener('click', clickHandler);
+}
+
+
+// console.log("done setting handler");
