a231e35d38a4d9bb20183a0c07831776ba4b163f
eunicelok <eunicelp511@gmail.com>
2019-12-18 20:34:07 +0800

    Did part 2, check for win condition but did not specify is player 1 ("X") or player 2 ("O") wins

--- script.js
+++ script.js
@@ -1,20 +1,7 @@
 console.log("Great!");
-//Assumption, when game is empty, first icon appear is x. Then next is o. Switching between x and o.
-//if user click 1st button, x will appear
-//next user clicks, o will appear
 
 
-
-//If (box [0][0] === “x” && box[0][1] === “x” && box[0][2] === “x”)
-//console.log(“x across the top”)
-//Instructions
-//Create a working game
-// create a very simple version of the game that users can play. When a user clicks on the first the empty square it turns to an X. Then the turn after it turns to an O. Then switches back, etc.
-var board = ["", "", "",
-            "" , "", "",
-            "", "", ""]; //for later to store value into board
-
-var turnsX = document.querySelectorAll(".box"); //created var turnsX that stores the array of box classes
+var boxes = document.querySelectorAll(".box"); //created var boxes that stores the array of box classes
 
 var X = true; //setting the starting value as true first
 
@@ -23,53 +10,77 @@ var turnedX = function() {
     if (this.innerHTML === "" && X === true) {
         this.innerHTML = "X";
         X = false;
+        checkForWin(); //run checkForWin function after X = false;
     } else if (this.innerHTML === "" && X !== true) {
         this.innerHTML = "O";
         X = true;
+    checkForWin();
     }
     console.log("Box turned X");
 }
 // this function is when we call, on clicking the box, the box will turn
 var clickFunc = function(event) {
-    for (var i = 0; i < turnsX.length; i++) {
-        turnsX[i].addEventListener("click", turnedX);
+    for (var i = 0; i < boxes.length; i++) {
+        boxes[i].addEventListener("click", turnedX);
 };
 
  };
 clickFunc();
 
+
 // this.innerHTML use this for all the innerHTML
 
+// winning state is either three "X" or three "O" in either row, column, or diagonal.
+// 0, 1, 2
+// 3, 4, 5
+// 6, 7, 8 boxes's array
 
-// The simplest implementation of this game is simply 9 buttons or divs with click events attached to each of them. Clicking on a square just changes the text of the element to X or O. After 9 moves are played the game doesn't do anything else. If the users want to continue playing they can refresh the page.
+var checkForWin = function() {
+
+switch(true) {
 
 
-// The first version of the game can just be a grid - no need to create the tictactoe board faithfully.
+   case (boxes[0].innerHTML !== "" && boxes[0].innerHTML===boxes[1].innerHTML && boxes[1].innerHTML=== boxes[2].innerHTML):
+     alert("You win!");
+     console.log("1");
+     break;
 
-// When the user clicks each button it sets a global variable that holds the current player. (starts as a null value when the game loads, is x after the first turn, then switches between players)
+     case (boxes[3].innerHTML !== "" && boxes[3].innerHTML===boxes[4].innerHTML && boxes[4].innerHTML=== boxes[5].innerHTML) :
+     alert("You win!");
+     console.log("2");
+     break;
 
-// var playerTurn = null;
+    case (boxes[6].innerHTML !== "" && boxes[6].innerHTML===boxes[7].innerHTML && boxes[7].innerHTML=== boxes[8].innerHTML) :
+    alert("You win!");
+console.log("3");
+    break;
 
-// var player1Turn = "X";
-// var player2Turn = "0";
+    case (boxes[0].innerHTML !== "" && boxes[0].innerHTML===boxes[3].innerHTML && boxes[3].innerHTML=== boxes[6].innerHTML) :
+    alert("You win!");
+console.log("4");
+    break;
 
-// if (playerTurn === null || player2Turn === "0") {
-//     player1Turn;
-//     player2Turn
-// } else (playerTurn === player1Turn) {
-//     player2Turn;
-//     player1Turn
-// }
+    case (boxes[1].innerHTML !== "" && boxes[1].innerHTML===boxes[4].innerHTML && boxes[4].innerHTML=== boxes[7].innerHTML) :
+    alert("You win!");
+console.log("5");
+    break;
 
-// if player turn is null or player turn is o
-//   player turn is x
-// else
-//   player turn is o
+    case (boxes[2].innerHTML !== "" && boxes[2].innerHTML===boxes[5].innerHTML && boxes[5].innerHTML=== boxes[8].innerHTML) :
+    alert("You win!");
+console.log("6");
+    break;
 
-// var board = [
-//   [ null, null, null ],
-//   [ null, null, null ],
-//   [ null, null, null ]
-// ];
+    case (boxes[0].innerHTML !== "" && boxes[0].innerHTML===boxes[4].innerHTML && boxes[4].innerHTML=== boxes[8].innerHTML) :
+    alert("You win!");
+console.log("7");
+    break;
 
-// so that the starting value is zero
+    case (boxes[2].innerHTML !== "" && boxes[2].innerHTML===boxes[4].innerHTML && boxes[4].innerHTML=== boxes[6].innerHTML) :
+    alert("You win!");
+console.log("8");
+    break;
+  default:
+// alert("Nobody wins")
+    // code block
+};
+};
