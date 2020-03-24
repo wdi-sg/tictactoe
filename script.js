b6550b33cf3264495a5601745a886245a820772d
osbornechan <osbornecab@gmail.com>
2020-03-24 22:04:31 +0800

    Add second version

--- script.js
+++ script.js
@@ -1,33 +1,48 @@
-var firstRowLeft = document.querySelector('.first-row-left');
-var firstRowCenter = document.querySelector('.first-row-center');
-var firstRowRight = document.querySelector('.first-row-right');
-var secondRowLeft = document.querySelector('.second-row-left');
-var secondRowCenter = document.querySelector('.second-row-center');
-var secondRowRight = document.querySelector('.second-row-right');
-var thirdRowLeft = document.querySelector('.third-row-left');
-var thirdRowCenter = document.querySelector('.third-row-center');
-var thirdRowRight = document.querySelector('.third-row-right');
+//Creating main div container
+var mainDivContainer = document.createElement('div');
+mainDivContainer.classList.add('main-container');
+document.body.appendChild(mainDivContainer);
 
 var playerTurn = 'X';
 
 var clickHandler = function (event) {
     var elementSelected = document.querySelector('.' + event.target.className);
 
-    if (playerTurn == 'X') {
+    if (playerTurn == 'X' && elementSelected.innerText == '') {
         playerTurn = 'O';
         elementSelected.innerText = 'X';
-    } else {
+    } else if (playerTurn == 'O' && elementSelected.innerText == '') {
         playerTurn = 'X';
         elementSelected.innerText = 'O';
     }
 }
 
-firstRowLeft.addEventListener("click", clickHandler);
-firstRowCenter.addEventListener("click", clickHandler);
-firstRowRight.addEventListener("click", clickHandler);
-secondRowLeft.addEventListener("click", clickHandler);
-secondRowCenter.addEventListener("click", clickHandler);
-secondRowRight.addEventListener("click", clickHandler);
-thirdRowLeft.addEventListener("click", clickHandler);
-thirdRowCenter.addEventListener("click", clickHandler);
-thirdRowRight.addEventListener("click", clickHandler);
+//Creating the grid
+var gridCreator = function (num) {
+    for (var i = 0; i < num; i++) {
+        var rowDiv = document.createElement('div');
+        rowDiv.classList.add('row');
+        mainDivContainer.appendChild(rowDiv);
+
+        for (var j = 0; j < num; j++) {
+            var columnDiv = document.createElement('div');
+            columnDiv.classList.add('square-' + i + '-' + j);
+            columnDiv.addEventListener("click", clickHandler);
+            rowDiv.appendChild(columnDiv);
+
+        }
+    }
+}
+
+gridCreator(3);
+
+
+var firstRowLeft = document.querySelector('.square-0-0');
+var firstRowCenter = document.querySelector('.square-0-1');
+var firstRowRight = document.querySelector('.square-0-2');
+var secondRowLeft = document.querySelector('.square-1-0');
+var secondRowCenter = document.querySelector('.square-1-1');
+var secondRowRight = document.querySelector('.square-1-2');
+var thirdRowLeft = document.querySelector('.square-2-0');
+var thirdRowCenter = document.querySelector('.square-2-1');
+var thirdRowRight = document.querySelector('.square-2-2');
