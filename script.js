a523bc02e1362cf48c7fa6ecc6c0ea6f7e2660a2
travisenquiry <travisenquiry@gmail.com>
2020-03-24 20:34:45 +0800

    second version completed

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 var turn = 1;
+var counter = 0;
+
+var board = document.createElement("div");
+board.id = "board";
+document.body.appendChild(board);
+
+for (var i = 0; i < 3; i++){
+  var gameRow = document.createElement("div");
+  gameRow.className = "game-row";
+  board.appendChild(gameRow);
+  for(var j = 0; j < 3; j++){
+    var gameSquare = document.createElement("span");
+    gameSquare.className = "game-square";
+    gameSquare.innerHTML = "- ";
+    gameRow.appendChild(gameSquare);
+  }
+}
 
 var flipSymbol = function(event){
   if(turn === 1){
