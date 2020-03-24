57015e59c3dd15e959f54feb449daad1ed3d8c20
BOBBY <BOBBYKWONGZW@GMAIL.COM>
2020-03-24 20:15:04 +0800

    create board from javascript

--- script.js
+++ script.js
@@ -3,7 +3,9 @@
 let playerTurn = true;
 
 //Assigning event listener to boxes
-const boxes = document.querySelectorAll('.square');
+// const boxes = document.querySelectorAll('.square');
+
+//click Handle
 
 const clickHandler = (event) => {
     if (playerTurn === true){
@@ -18,8 +20,27 @@ const clickHandler = (event) => {
     }
 };
 
+
+// Creating board
+
 let i = 0;
-while(i<boxes.length){
-    boxes[i].addEventListener('click', clickHandler);
+while (i < 3) {
+    const body = document.querySelector('body');
+    const row = document.createElement('div');
+    row.className = "row";
+    body.appendChild(row);
+    for (let e=0; e<3; e++){
+        const box = document.createElement('div');
+        box.className = "box";
+        box.addEventListener('click', clickHandler);
+        row.appendChild(box);
+    }
     i++;
 }
+
+
+// let i = 0;
+// while(i<boxes.length){
+//     boxes[i].addEventListener('click', clickHandler);
+//     i++;
+// }
