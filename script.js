46cc84b96fdd9b5a2c9710132353f7d04dbe5b95
YusofGotboudine <yusof95@hotmail.com>
2020-03-25 03:46:39 +0800

    toetactic by Yusof

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log("hello script js");
+
+var inputHappened = function (currentInput) {
+  console.log(currentInput);
+  // var output = "WOW SOMETHING HAPPENED";
+  display(currentInput);
+  // return output;
+};
+
+var display = function (data) {
+  // var hOne = document.getElementByTagName("h1");
+  // hOne.innerText = data;
+  // hOne.addEventListener('mouseover', saySomething);
+};
+
+// var saySomething = function () {
+//   console.log("YES!");
+// };
