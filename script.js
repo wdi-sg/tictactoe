f2a50dfd89ed9cc3da1172f4ba1048b163dead87
dannst <arcanic@gmail.com>
2020-03-25 02:07:40 +0800

    removed html elements. created board using javascript. enlarged board size.

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
 console.log('script is running');
 
+//board creation
+const boardCreate = function(){
+	let divBoard = document.createElement('div');
+	document.body.appendChild(divBoard);
+	divBoard.className = "board";
+
+	let rowOne = divBoard.appendChild(document.createElement('div'));
+	let rowTwo = divBoard.appendChild(document.createElement('div'));
+	let rowThree = divBoard.appendChild(document.createElement('div'));
+	rowOne.className = "row";
+	rowTwo.className = "row";
+	rowThree.className = "row";
+
+	rowOne.appendChild(document.createElement('button'));	
+	rowOne.appendChild(document.createElement('button'));	
+	rowOne.appendChild(document.createElement('button'));	
+
+	rowTwo.appendChild(document.createElement('button'));	
+	rowTwo.appendChild(document.createElement('button'));	
+	rowTwo.appendChild(document.createElement('button'));	
+
+	rowThree.appendChild(document.createElement('button'));	
+	rowThree.appendChild(document.createElement('button'));	
+	rowThree.appendChild(document.createElement('button'));	
+
+	// for (i = 0; i < 3; i++){
+	// 	document.querySelectorAll('.row')[0].childNodes[i].className = 'button';
+	// 	document.querySelectorAll('.row')[1].childNodes[i].className = 'button';
+	// 	document.querySelectorAll('.row')[2].childNodes[i].className = 'button';
+	// }
+}
 
 // keeps track of player 1 or 2
 let player_turn = 1;
@@ -22,3 +53,6 @@ document.body.addEventListener('click', function(event){
 		takeTurn();
 	}
 });
+
+
+boardCreate();
