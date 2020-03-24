56fb5d337f9c3482ea5024e1246bf9bbb0f378ff
Rachelik <rachel.ik.87@gmail.com>
2020-03-24 22:12:17 +0800

    deleted unwanted code

--- script.js
+++ script.js
@@ -1,24 +1,9 @@
 var userChoice = ["X", "O", ""];
-
 var gameSquares = document.querySelectorAll('.game-square');
 
 var player;
 var playerSymbol;
 var gameProgress = 0;
-
-var userChoiceToggle = function(){
-    for(var gameSquaresI = 0; gameSquaresI < gameSquares.length; gameSquaresI++) {
-        if(gameSquaresI % 2 === 0) {
-            player = "Player 1";
-            playerSymbol = userChoice[0];
-        }else if(gameSquaresI % 2 === 1){
-            player = "Player 2";
-            playerSymbol = userChoice[1];
-        }
-        return playerSymbol;
-    }
-}
-
 
 var gameSquaresClick = function(event){
     console.log('clicked');
@@ -32,7 +17,6 @@ var gameSquaresClick = function(event){
         gameProgress++;
     };
 };
-
 
 for (var gameSquaresI = 0; gameSquaresI < gameSquares.length; gameSquaresI++){
     playerClick = gameSquares[gameSquaresI].addEventListener('click', gameSquaresClick);
