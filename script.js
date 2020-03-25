0fd04c99ad32bf1647dce01830585248c868fbb4
elisaes <alisa260785@gmail.com>
2020-03-26 03:31:59 +0800

    input

--- script.js
+++ script.js
@@ -1,59 +1,101 @@
 //variable declaration
-let counter = 0;
+let turn = 0;
 let boardSize = 3;
-let arr = [];
+let newArr = [];
+let xPlayer = [];
+let arrDatai =[];
+let arrDataj = [];
+let yPlayer = [];
+let currentInput;
 
 //input the values from the user
 
 const inputHandler = function(event) {
-
-  console.log(event);
+  //console.log(event);
   const currentInput = event.target.id;
   inputHappened(currentInput);
-  const dataId = this.getAttribute("data-id");
-  console.log(dataId);
-  console.log(typeof(dataId));
-  const numberDataId = parseInt(dataId);
-  console.log(numberDataId);
-  console.log(typeof(numberDataId));
-  arr = currentInput[dataId]
-   };
 
-//console.log("arr", arr);1
+  //gettin the id when is clicked
+  const datai = this.getAttribute("data-i");
 
-//create the arrays
-const createArrays = function(event) {
-  for (let i = 0; i < boardSize; i++) {
-    for (let j = 0; j < boardSize; j++) {
-       // creating Element in the array
-       
-            arr = document.createElement("input");
-      //console.log ("value of the arr",arr)
-      arr.setAttribute("data-id", [i] + " " + [j]);
-      arr.setAttribute("class", 'rows');
-      document.body.appendChild(arr);
-      arr.addEventListener("click", inputHandler);
-      console.log(arr)
+
+  const dataj = this.getAttribute("data-j");
+  console.log(datai, dataj);
+  arrDatai.push(datai);
+    arrDataj.push(dataj);
+  //this.addEventListener("click", onclick);
+
+  
+  for (let i = 0; i < 3; i++) {
+    if (arrData[i])
+    console.log("Xwin");
+    //}
+  }
+  for (let j = 0; j < 3; j++) {
+    if (currentInput == "O") {
+      console.log("Ywin");
     }
   }
 };
 
+//   if ((currentInput === "X" && datai == 0) || (currentInput === "X" && datai == 1)||(currentInput==="X" && datai == 2) {
+//       xPlayer.push(datai,dataj);
+//     if (datai === "X" ||) console.log("newArr", newArr);
+//   } else{
+//       yPlayer.push(datai,dataj)
+//   }
+// };
 
+//console.log("arr", arr);1
+//create the arrays
+const createArrays = function(event) {
+  for (let i = 0; i < boardSize; i++) {
+    newArr[i] = [];
+    //console.log(newArr);
 
+    for (let j = 0; j < boardSize; j++) {
+      // creating Element in the array
+      let arrj = document.createElement("div");
+      //console.log(typeOf(arr));
+      arrj.setAttribute("data-i", i);
+      arrj.setAttribute("data-j", j);
+      arrj.setAttribute("class", "inside");
+      arrj.setAttribute("display", "auto");
+      arrj.addEventListener("click", inputHandler);
+      //   arrj.addEventListener("click", onclick);
+      document.querySelector(".container").appendChild(arrj);
+      newArr[i][j] = arrj;
+      //console.log(newArr[i]);
+    }
+  }
+};
+
+createArrays();
+//console.log(newArr);
+//document.body.appendChild(newArr);
 
 //button
-document.getElementById("starter").addEventListener("click", createArrays);
+//document.getElementById("starter").addEventListener("click", createArrays);
 
 //solving
 const inputHappened = function(currentInput) {
-   
-    if (counter % 2 === 0) {
-        currentInput = "X";
-             } else {
-        currentInput = "O";
-      }
-      
-  console.log("currentInput", currentInput);
-  counter = counter + 1;
-};
+  if (turn < 9) {
+    if (turn % 2 === 0) {
+      currentInput = "X";
+      xPlayer.push(currentInput);
+    } else {
+      currentInput = "O";
+    }
+  } else {
+    yPlayer.push(currentInput);
+    console.log("game over");
+  }
 
+  // newArr = currentInput[dataId];
+  // console.log("currentInput", currentInput);
+  turn = turn + 1;
+  currentInput.display = currentInput;
+  //   onclick = function() {
+  //     document.getElementsByClassName('inside').innerHTML.display = currentInput;
+  //   };
+};
