8c8f1de94d1dcf01332077e47db7cc62b21cedb3
kokchuantan <kokchuan1996@gmail.com>
2020-03-25 19:11:50 +0800

    updated win condition

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 console.log('test')
-var x = totalClick =i = j= 0;
+var x = totalClick = i = j = 0;
 var playerIcon = '';
 var player1Icon = 'images/pikachu.png';
 var player2Icon = 'images/psyduck.png';
 player = true;
+var currentPlayer = '';
 var board = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null]
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
 ];
 var totalBoxes = document.querySelectorAll('.box');
 
@@ -17,67 +18,88 @@ var setClick = function () {
     }
 }
 
+var addToBoardArray = function (input) {
+    // for (i = 0; i < board.length; i++) {
+    //     for (j = 0; j < board.length; j++) {
+    //         if (true) {
+    //             console.log('4');
+    //             board[i][j] = currentPlayer;
+    //             console.log(currentPlayer + ' = currentPlayer')
+    //             console.log(board[i][j] + 'board');
+
+    //         }
+    //     }
+    // }
+}
+
+var checkWin = function () {
+    if (board[0][0] === currentPlayer && board[0][1] === currentPlayer && board[0][2] === currentPlayer) {
+        alert("You won!");
+        return;
+    } else if (board[1][0] === currentPlayer && board[1][1] === currentPlayer && board[1][2] === currentPlayer) {
+        alert("You won!");
+        return;
+    } else if (board[2][0] === currentPlayer && board[2][1] === currentPlayer && board[2][2] === currentPlayer) {
+        alert("You won!");
+        return;
+    } else if (board[0][0] === currentPlayer && board[1][0] === currentPlayer && board[2][0] === currentPlayer) {
+        alert("You won!");
+        return;
+    } else if (board[0][1] === currentPlayer && board[1][1] === currentPlayer && board[2][1] === currentPlayer) {
+        alert("You won!");
+        return;
+    } else if (board[0][2] === currentPlayer && board[1][2] === currentPlayer && board[2][2] === currentPlayer) {
+        alert("You won!");
+        return;
+    } else if (board[0][2] === currentPlayer && board[1][1] === currentPlayer && board[2][0] === currentPlayer) {
+        alert("You won!");
+        return;
+    } else if (board[0][0] === currentPlayer && board[1][1] === currentPlayer && board[2][2] === currentPlayer) {
+        alert("You won!");
+        return;
+    } else {
+        console.log("nothing")
+        return;
+    }
+
+}
+
+var checkPlayer = function (target) {
+    var temp = target;
+    var tempId = temp.id;
+    var input = tempId.split('-');
+    var input1 = parseInt(input[0])
+    var input2 = parseInt(input[1])
+    if (player) {
+        player = false;
+        playerIcon = player1Icon;
+        currentPlayer = 'player1';
+        board[input1][input2] = currentPlayer;
+        checkWin(currentPlayer)
+    } else {
+        player = true;
+        playerIcon = player2Icon;
+        currentPlayer = 'player2';
+        board[input1][input2] = currentPlayer;
+        checkWin(currentPlayer)
+    }
+}
+
 var onClick = function (event) {
+    //console.log(event + ' = event');
     var userInput = event.target;
-    checkPlayer();
+    //console.log(userInput + ' = userInput');
+    //console.log(event.target + ' = event.target')
+    checkPlayer(userInput);
     var img = document.createElement('img');
     img.src = playerIcon;
     img.innerHTML = '';
     userInput.appendChild(img);
 };
 
-var checkPlayer = function () {
-    if (player) {
-        player = false;
-        playerIcon = player1Icon;
-        addToBoardArray(this.innerHTML)
-        checkWin(this.innerHTML)
-    } else {
-        player = true;
-        playerIcon = player2Icon;
-        addToBoardArray(this.innerHTML)
-        checkWin(this.innerHTML)
-    }
-}
-
-
-var addToBoardArray = function (input) {
-    console.log(input)
-    let z = input;
-    console.log(z)
-    for (i = 0; i < board.length; i++) {
-        for (j = 0; j < board.length; j++) {
-            if(totalBoxes[j].innerHTML === input){
-                console.log('4');
-                board[i][j] = (input);
-                console.log(board[i][j] + 'board');
-            }
-        } 
-    }
-}
-
-checkWin = function () {
-    if (board[0][0] === board[0][1] === board[0][2]) {
-        alert("You won!");
-    } else if (board[1][0] === board[1][1] === board[1][2]) {
-        alert("You won!");
-    } else if (board[2][0] === board[2][1] === board[2][2]) {
-        alert("You won!");
-    } else if (board[0][0] === board[1][0] === board[2][0]) {
-        alert("You won!");
-    } else if (board[0][1] === board[1][1] === board[2][1]) {
-        alert("You won!");
-    } else if (board[0][2] === board[1][2] === board[2][2]) {
-        alert("You won!");
-    } else if (board[0][2] === board[1][1] === board[2][0]) {
-        alert("You won!");
-    } else if (board[0][0] === board[1][1] === board[2][2]) {
-        alert("You won!");
-    } else {
-        console.log("nothing")
-    }
-
-}
+var reset = document.createElement('button');
+reset.innerText = 'Reset';
+document.body.appendChild(reset)
 
 
 
