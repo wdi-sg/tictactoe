1ae5d17ce8acbab9050f24f59aaa4262be9d64ba
wickker <choydianchun@gmail.com>
2020-03-25 12:01:23 +0800

    added start button

--- script.js
+++ script.js
@@ -1,36 +1,44 @@
 //variables
-let gameboard = document.getElementById("gameboard");
-let gamePlayArr = [
-  ["", "", ""],
-  ["", "", ""],
-  ["", "", ""]
-];
-let outputCount = 1;
+let gameboardDiv = document.getElementById("gameboard");
+let buttonDiv = document.getElementById("gamebutton");
+let gamePlayArr = [];
+let outputCount = 0;
+let boardSize = 3;
+let gameButtonState = 0;
 
 //initialise game board
+//1. initialise game play array that contains empty string
+//2. creates game play board display via DOM
+//3. adds event listener to each grid element of board
 function initGameBoard() {
-  for (let i = 0; i < 3; i++) {
-    for (let x = 0; x < 3; x++) {
+  for (let i = 0; i < boardSize; i++) {
+    gamePlayArr.push([]);
+    for (let x = 0; x < boardSize; x++) {
+      gamePlayArr[i].push("");
       let newDiv = document.createElement("div");
       newDiv.className = "square";
       newDiv.textContent = "";
       newDiv.id = i + "-" + x;
-      gameboard.appendChild(newDiv);
+      gameboardDiv.appendChild(newDiv);
       console.log(newDiv.id);
       newDiv.addEventListener("click", onClick);
     }
   }
+  startGameButtonErase();
 }
 
 function onClick(event) {
-  if (event.target.textContent !== "") {
+  if (event.target.textContent !== "" || gameButtonState === 0) {
     return;
   }
+  //add: if win condition, add start game button, return
   let clickedIndex = event.target.id.split("-");
   console.log(clickedIndex[0]);
   console.log(clickedIndex[1]);
   let xCoor = clickedIndex[0];
   let yCoor = clickedIndex[1];
+  outputCount++;
+  console.log(outputCount);
   if (outputCount % 2 !== 0) {
     gamePlayArr[xCoor][yCoor] = "X";
     event.target.textContent = "X";
@@ -38,7 +46,25 @@ function onClick(event) {
     gamePlayArr[xCoor][yCoor] = "O";
     event.target.textContent = "O";
   }
-  outputCount++;
+}
+
+function startGameButtonErase() {
+  buttonDiv.addEventListener("click", function(event) {
+    buttonDiv.removeChild(event.target);
+    console.log(gameButtonState);
+    console.log(event.target);
+    gameButtonState = 1;
+    console.log(gameButtonState);
+  });
+}
+
+function initGameButton() {
+  let newButton = document.createElement("button");
+  newButton.textContent = "START GAME";
+  buttonDiv.appendChild(newButton);
+  gameButtonState = 0;
 }
 
 initGameBoard();
+
+
