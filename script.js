165a41b68183d09303e60fbc37f4cdc1b88f365f
ThomasOh92 <ohcsthomas@gmail.com>
2020-03-25 22:12:57 +0800

    Further Functionality 1 - Added win condition

--- script.js
+++ script.js
@@ -1,24 +1,82 @@
+let body = document.querySelector('body');
+let winStatement = document.createElement("p");
+winStatement.id = "win-statement"
+
+let player = "X";
+
+
+//Check Win Function
+var someoneHasWon = () => {
+    //Check for rows
+    for (let i = 0; i < 3; i++){
+        let matchCounter = 0;
+        for (let k = 0; k < 3; k++){
+            if (gameState[i][k] === player){
+                matchCounter++
+            }
+        }
+        if (matchCounter === 3){
+            return true
+        }
+    }
+
+
+
+}
+
 //Function to select grid item and change it to "X" or "O"
-var turnForX = true;
-var selectItem = function(){
-    if (turnForX){
+let selectItem = function(){
+    if (player === "X"){
+        //Update DOM
         this.innerText = "X"
-        turnForX = false;
+
+        //Update Game State
+        let idArray = this.id.split(",").map(x => parseInt(x));
+        gameState[idArray[0]][idArray[1]] = "X";
+
+        //Check win condition
+        if (someoneHasWon()){
+            winStatement.innerText = `Player ${player} has won!`
+            body.appendChild(winStatement);
+        }
+
+        //Change Turn
+        player = "O";
+
     } else{
+        //Update DOM
         this.innerText = "O"
-        turnForX = true;
+
+        //Update Game State
+        let idArray = this.id.split(",").map(x => parseInt(x));
+        gameState[idArray[0]][idArray[1]] = "O";
+
+        //Check win condition
+        if (someoneHasWon()){
+            winStatement.innerText = `Player ${player} has won!`
+            body.appendChild(winStatement);
+        }
+
+        //Change Turn
+        player = "X";
     }
 }
 
-//Generate Game Board
-var body = document.querySelector('body');
-var gameContainer = document.createElement('div');
+//Multi purpose loop to generate Game Board and Game State
+let gameContainer = document.createElement('div');
+let gameState = [];
 gameContainer.className = "container";
 
-for (let i = 0; i < 9; i++){
-    var gridItem = document.createElement('div');
-    gridItem.className = "grid-item";
-    gridItem.addEventListener("click", selectItem);
-    gameContainer.appendChild(gridItem);
+for (let i = 0; i < 3; i++){
+    let gameStateRow = [];
+    gameState.push(gameStateRow);
+    for (let k = 0; k < 3; k++){
+        let gridItem = document.createElement('div');
+        gridItem.className = "grid-item";
+        gridItem.id = i + "," + k;
+        gridItem.addEventListener("click", selectItem);
+        gameContainer.appendChild(gridItem);
+        gameStateRow.push(null);
+    }
 }
 body.appendChild(gameContainer);
