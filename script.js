e049762b684f6a8b6ced22ab1e6d1541d72b55f0
qundax <qunda.lim@outlook.sg>
2020-03-25 19:39:11 +0800

    sei-22-qunda - Added start game button - Added ability to add player name - Added ability to keep track of player score - Added ability to reset score

--- script.js
+++ script.js
@@ -6,8 +6,18 @@ var board = [
 [null, null, null]
 ]
 
+var playerOne = "Player 1";
+var playerTwo = "Player 2";
+
+var playerOneScoreDisplay = document.getElementById("player-one-score");
+var playerTwoScoreDisplay = document.getElementById("player-two-score");
+
+var playerOneScore = parseInt(playerOneScoreDisplay.innerText);
+var playerTwoScore = parseInt(playerTwoScoreDisplay.innerText);
+
 var playerTurn = 1;
 var moves = 0;
+var gameStart = false;
 
 var gameboard = document.querySelector(".gameboard");
 var rows = gameboard.children;
@@ -19,21 +29,36 @@ for (let i = 0; i < rows.length; i++) {
   }
 }
 
+var startButton = document.getElementById("start");
+startButton.addEventListener("click", startGame);
+
 var clearButton = document.getElementById("clear");
 clearButton.addEventListener("click", clearBoard);
+
+var resetButton = document.getElementById("reset");
+resetButton.addEventListener("click", resetScore);
 
 
 
 // Helper functions
 
+function startGame() {
+  gameStart = true;
+  startButton.style.display = "none";
+  playerOne = document.querySelectorAll("input")[0].value;
+  playerTwo = document.querySelectorAll("input")[1].value;
+}
+
 function clickHandler(event) {
-  if (moves === 9) {
-    alert("You cannot make any more moves");
-    return
+  if (gameStart) {
+    if (moves === 9) {
+      alert("You cannot make any more moves");
+      return
+    }
+    addSymbol(event);
+    checkWin();
+    moves++;
   }
-  addSymbol(event);
-  checkWin();
-  moves++;
 }
 
 function addSymbol(event) {
@@ -54,14 +79,54 @@ function clearBoard() {
   }
   playerTurn = 1;
   moves = 0;
+  gameStart = false;
+  startButton.style.display = "inline";
+  document.querySelectorAll("input")[0].value = "";
+  document.querySelectorAll("input")[1].value = "";
+}
+
+function resetScore() {
+  playerOneScoreDisplay.innerText = "0";
+  playerTwoScoreDisplay.innerText = "0";
 }
 
 function checkWin() {
   for (let i = 0; i < board.length; i++) {
+    // row win
     if (board[i][0].innerText === "X" && board[i][1].innerText === "X" && board[i][2].innerText === "X") {
-      alert("Player 1 won!");
+      alert(`${playerOne} won!`);
+      playerOneScore++;
+      playerOneScoreDisplay.innerText = playerOneScore;
     } else if (board[i][0].innerText === "O" && board[i][1].innerText === "O" && board[i][2].innerText === "O") {
-      alert("Player 2 won!");
-    }
+      alert(`${playerTwo} won!`);
+      playerTwoScore++;
+      playerTwoScoreDisplay.innerText = playerTwoScore;
+    // diagonal win
+  } else if (board[0][0].innerText === "X" && board[1][1].innerText === "X" && board[2][2].innerText === "X") {
+    alert(`${playerOne} won!`);
+    playerOneScore++;
+    playerOneScoreDisplay.innerText = playerOneScore;
+  } else if (board[0][0].innerText === "O" && board[1][1].innerText === "O" && board[2][2].innerText === "O") {
+    alert(`${playerTwo} won!`);
+    playerOnTwoore++;
+    playerTwoScoreDisplay.innerText = playerTwoScore;
+  } else if (board[0][2].innerText === "X" && board[1][1].innerText === "X" && board[2][0].innerText === "X") {
+    alert(`${playerOne} won!`);
+    playerOneScore++;
+    playerOneScoreDisplay.innerText = playerOneScore;
+  } else if (board[0][2].innerText === "O" && board[1][1].innerText === "O" && board[2][0].innerText === "O") {
+    alert(`${playerTwo} won!`);
+    playerOnTwoore++;
+    playerTwoScoreDisplay.innerText = playerTwoScore;
+    // column win
+  } else if (board[0][i].innerText === "X" && board[1][i].innerText === "X" && board[2][i].innerText === "X") {
+    alert(`${playerOne} won!`);
+    playerOneScore++;
+    playerOneScoreDisplay.innerText = playerOneScore;
+  } else if (board[0][i].innerText === "O" && board[1][i].innerText === "O" && board[2][i].innerText === "O") {
+    alert(`${playerTwo} won!`);
+    playerOnTwoore++;
+    playerTwoScoreDisplay.innerText = playerTwoScore;
   }
+}
 }
