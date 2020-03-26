3ec14309d97fbe6deb2bbf646739bd79f44f83fc
marctanyh <marctanyh@gmail.com>
2020-03-26 22:48:25 +0800

    Marcus -ticktacks - Part 2 wip
    more than it was yesterday

--- script.js
+++ script.js
@@ -16,77 +16,106 @@ squares.innerHTML = " ";
 
 
 var board = [
-        null, null, null,
-        null, null, null,
-        null, null, null
+        [null, null, null,],
+        [null, null, null,],
+        [null, null, null,],
 ];
 
 var steps = 0;
 
-changedSquare();
-
+console.log(steps);
 
 var message = document.querySelector(".output");
 
-var clickedSquare = function (event) {
-    var clickedElement = event.target;
-    if (steps === 0) {
-        steps = steps + 1;
-        clickedElement.innerHTML = "X";
-    } else if (steps === 1) {
-        steps = steps - 1;
-        clickedElement.innerHTML = "O";
+var checkWin = function (currentPlayer) {
+    console.log(board);
+
+    if (board[0][0] === currentPlayer && board[0][1] === currentPlayer && board[0][2] === currentPlayer) {
+        alert(currentPlayer + " has won!");
+
+        return ;
+    } else if (board[1][0] === currentPlayer && board[1][1] === currentPlayer && board[1][2] === currentPlayer) {
+        alert(currentPlayer + " has won!");
+
+        return;
+    } else if (board[2][0] === currentPlayer && board[2][1] === currentPlayer && board[2][2] === currentPlayer) {
+        alert(currentPlayer + " has won!");
+
+        return;
+    } else if (board[0][0] === currentPlayer && board[1][0] === currentPlayer && board[2][0] === currentPlayer) {
+        alert(currentPlayer + " has won!");
+
+        return;
+    } else if (board[0][1] === currentPlayer && board[1][1] === currentPlayer && board[2][1] === currentPlayer) {
+        alert(currentPlayer + " has won!");
+
+        return;
+    } else if (board[0][2] === currentPlayer && board[1][2] === currentPlayer && board[2][2] === currentPlayer) {
+        alert(currentPlayer + " has won!");
+
+        return;
+    } else if (board[0][2] === currentPlayer && board[1][1] === currentPlayer && board[2][0] === currentPlayer) {
+        alert(currentPlayer + " has won!");
+
+        return ;
+    } else if (board[0][0] === currentPlayer && board[1][1] === currentPlayer && board[2][2] === currentPlayer) {
+        alert(currentPlayer + " has won!");
+        return;
+    } else if (steps === 9) {
+        alert("It is a draw!");
+
+        return;
     }
+}
+
+var clickedSquare = function (event) {
+
+    var clickedElement = event.target;
+
+    // if (event.target.innerHTML !== '') {
+    //     return
+    // }
+
+    if (steps % 2 === 0) {
+        steps = steps + 1;
+
+        clickedElement.innerHTML = "X";
+        checkWin("X")
+        console.log(steps)
+    } else if (steps % 2 === 1) {
+        steps = steps + 1;
+        clickedElement.innerHTML = "O";
+        checkWin("O")
+        console.log(steps)
+    }
+
+// function checkWin (parameter1, parameter2) {
+//     console.log(parameter1, parameter2)
+// }
+
+// checkWin(87, "world")
+// checkWin("fuck", "you")
+
+    // switch (steps) {
+    //     case 0:
+    //         steps = steps + 1;
+    //         clickedElement.innerHTML = "X";
+    //         break
+    //     case 1:
+    //         steps = steps - 1;
+    //         clickedElement.innerHTML = "O";
+    //         break
+    // }
 };
 
 
-var changedSquare = function (selectedSquare, clickedSqaure) {
-    for (var i = 0; i < board.length; i++) {
+var changedSquare = function () {
+    for (var i = 0; i < squares.length; i++) {
         var selectedSquare = squares[i];
         selectedSquare.addEventListener('click', clickedSquare);
     }
 }
 
-console.log("yo you still workin");
-
-// var checkWin = function () {
-//     if (board[0][0] === currentPlayer && board[0][1] === currentPlayer && board[0][2] === currentPlayer) {
-//         alert(currentPlayer + " has won!");
-//         begin = true;
-//         return ;
-//     } else if (board[1][0] === currentPlayer && board[1][1] === currentPlayer && board[1][2] === currentPlayer) {
-//         alert(currentPlayer + " has won!");
-//         begin = true;
-//         return;
-//     } else if (board[2][0] === currentPlayer && board[2][1] === currentPlayer && board[2][2] === currentPlayer) {
-//         alert(currentPlayer + " has won!");
-//         begin = true;
-//         return;
-//     } else if (board[0][0] === currentPlayer && board[1][0] === currentPlayer && board[2][0] === currentPlayer) {
-//         alert(currentPlayer + " has won!");
-//         begin = true;
-//         return;
-//     } else if (board[0][1] === currentPlayer && board[1][1] === currentPlayer && board[2][1] === currentPlayer) {
-//         alert(currentPlayer + " has won!");
-//         begin = true;
-//         return;
-//     } else if (board[0][2] === currentPlayer && board[1][2] === currentPlayer && board[2][2] === currentPlayer) {
-//         alert(currentPlayer + " has won!");
-//         begin = true;
-//         return;
-//     } else if (board[0][2] === currentPlayer && board[1][1] === currentPlayer && board[2][0] === currentPlayer) {
-//         alert(currentPlayer + " has won!");
-//         begin = true;
-//         return ;
-//     } else if (board[0][0] === currentPlayer && board[1][1] === currentPlayer && board[2][2] === currentPlayer) {
-//         alert(currentPlayer + " has won!");
-//         begin = true;
-//         return;
-//     } else if (turnCount === 9) {
-//         alert("It is a draw!");
-//         begin = true;
-//         return;
-//     }
-// }
-
 changedSquare();
+
+console.log("yo you still workin");
