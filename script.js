607c90fa1f8638b660835770980efa54df830a8b
akjw <aseykoh@gmail.com>
2020-06-17 19:59:36 +0800

    add names, choose symbol, show score

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 //HTML Elements
 var resetButton = document.querySelector('#restart');
+var startButton = document.querySelector('#start');
+var scoreboard = document.querySelector('.scoreboard');
+var xScore = document.getElementById('x-score');
+var oScore = document.getElementById('o-score');
+var input = document.getElementById('input');
+var grid = document.querySelector('.grid');
 var cells = document.querySelectorAll('.cell');
 var startMessage = document.querySelector('.start-message');
 var startDisplay = document.querySelector('.start-state');
-startButton.addEventListener('click', newGame);
 var winMessage = document.querySelector('.outcome-message');
 var winDisplay = document.querySelector('.win-state');
 resetButton.addEventListener('click', newGame);
+var clickCount = 0;
+var playerOne = {};
+var playerTwo = {};
 
 //Game variables
 var xTurn = true;
 var xClass = 'x';
 var oClass = 'o';
+var xWinNum = 0;
+var oWinNum = 0;
 var possibleWins = [
     //horizontal
     [0, 1, 2],
@@ -26,10 +36,72 @@ var possibleWins = [
     [2, 4, 6]
 ]
 
-newGame();
+if (startButton) {
+    startButton.addEventListener('click', function(event){
+        var letters = /^[A-Za-z]+$/;
+        clickCount += 1
+        //First input = player 1 name
+        if (clickCount === 1){
+            //validate input
+            if (input.value === "" || !input.value.match(letters)) { 
+                startMessage.innerText = 'Please enter valid name!'
+                input.value = "";
+            } 
+            else {
+                playerOne.name = input.value;
+                console.log(playerOne);
+                input.placeholder = 'Enter name for player 2';
+                input.value = "";
+            }
+
+        } 
+        //Second input = player 2 name
+        else if (clickCount === 2) {
+            if (input.value === "" || !input.value.match(letters)) { 
+                startMessage.innerText = 'Please enter valid name!'
+                input.value = "";
+            }
+            else {
+                playerTwo.name = input.value;
+                input.placeholder = 'Choose symbol for player 1: X or O?';
+                input.value = "";
+            }
+        }
+        //Third input = player 1's symbol
+        else if (clickCount === 3) {
+            if (input.value.toUpperCase() === "X"){
+                playerOne.x = true;
+                playerTwo.o = true;
+                xTurn = true;
+                startButton.remove();
+                input.remove();
+                startDisplay.classList.remove('show');
+                scoreboard.classList.add('show');
+                grid.classList.add('show');
+                newGame();
+            }
+            else if (input.value.toUpperCase() === "O"){
+                playerOne.o = true;
+                playerTwo.x = true;
+                xTurn = false;
+                startButton.remove();
+                input.remove();
+                startDisplay.classList.remove('show');
+                scoreboard.classList.add('show');
+                grid.classList.add('show');
+                newGame();
+            }
+            else {
+                startMessage.innerText = 'Please choose a valid symbol!'
+                input.value = "";
+            }
+        }
+        
+    });
+}
+
 
 function newGame (){
-    xTurn = true;
     cells.forEach(cell => {
         cell.classList.remove(xClass)
         cell.classList.remove(oClass)
@@ -87,12 +159,18 @@ function verifyWin (symbolClass){
 
 function gameOver(draw){
     if (draw){
-        winMessage.innerText = 'It\'s a draw!'; 
-    }
+        winMessage.innerText = 'It\'s a draw between ' + playerOne.name + ' and ' + playerTwo.name + '! Play again?';
+    } 
     else if(xTurn){
-       winMessage.innerText = 'Win goes to X!';
+        xWinNum += 1;
+        xScore.innerText = 'X Score: ' + xWinNum;
+        //Check if playerOne object has the x symbol so correct winner displays
+        winMessage.innerText = `${playerOne.x ? playerOne.name : playerTwo.name} wins! Play again?`
     } else {
-        winMessage.innerText = 'Win goes to O!';
+        oWinNum += 1;
+        oScore.innerText = 'O Score: ' + oWinNum;
+        //Check if playerOne object has the o symbol so correct winner displays
+        winMessage.innerText = `${playerOne.o ? playerOne.name : playerTwo.name} wins! Play again?`
     }
     winDisplay.classList.add('show');
 }
