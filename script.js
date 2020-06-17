9842bf1bc2cc8b4069df18825fee426ee6049267
Randalla <the.randall.lim@gmail.com>
2020-06-17 21:56:08 +0800

    Co-authored-by: fabienlimzk <fabienlimzk@gmail.com>

--- script.js
+++ script.js
@@ -1,38 +1,116 @@
 //---------------------------------PART ONE-------------------------------------
 
-var box1 = document.querySelector(".box1").addEventListener("click", clicked);
-var box2 = document.querySelector(".box2").addEventListener("click", clicked);
-var box3 = document.querySelector(".box3").addEventListener("click", clicked);
-var box4 = document.querySelector(".box4").addEventListener("click", clicked);
-var box5 = document.querySelector(".box5").addEventListener("click", clicked);
-var box6 = document.querySelector(".box6").addEventListener("click", clicked);
-var box7 = document.querySelector(".box7").addEventListener("click", clicked);
-var box8 = document.querySelector(".box8").addEventListener("click", clicked);
-var box9 = document.querySelector(".box9").addEventListener("click", clicked);
+// let box1 = document.querySelector(".box1").addEventListener("click", clicked);
+// let box2 = document.querySelector(".box2").addEventListener("click", clicked);
+// let box3 = document.querySelector(".box3").addEventListener("click", clicked);
+// let box4 = document.querySelector(".box4").addEventListener("click", clicked);
+// let box5 = document.querySelector(".box5").addEventListener("click", clicked);
+// let box6 = document.querySelector(".box6").addEventListener("click", clicked);
+// let box7 = document.querySelector(".box7").addEventListener("click", clicked);
+// let box8 = document.querySelector(".box8").addEventListener("click", clicked);
+// let box9 = document.querySelector(".box9").addEventListener("click", clicked);
 
-var player = "O"
 
-function clicked(event) {
-    var clickk = document.querySelector("." + event.target.className)
-    if (player == "X") {
-        player = "O"
-        clickk.innerHTML = "X"
-    } else{
-        clickk.innerHTML = "O"
-        player = "X"
-    }
+
+// function clicked(event) {
+//     let clickk = document.querySelector('.'+ event.target.className)
+//     if (player == "X") {
+//         player = "O"
+//         clickk.innerHTML = "X"
+//     } else{
+//         clickk.innerHTML = "O"
+//         player = "X"
+//     }
+// }
+
+// ---------------------------------PART TWO-------------------------------------
+
+// let board = ["","","","","","","","","",]
+//   let box1 = document.createElement("div")  			//create single row <p></p>
+//   box1.innerHTML = "x"
+//   pee.innerHTML = pineapplePen(stuffToDisplay) // show number of pineapples between <p></p>
+//   document.querySelector("#output").appendChild(pee)
+// } 
+
+
+
+
+
+//---------------------------------create box ------------------
+
+let playerTurn = "X";
+let body = document.querySelector("body");
+let container = document.createElement("div");
+container.className = "container";
+
+for (let i = 1; i <= 9; i++) {
+    
+    let grid = document.createElement('div');
+    grid.className = "grid";
+    grid.addEventListener("click", clicked);
+    container.appendChild(grid);
+    grid.id = "grid" + i;
+    console.log(grid.id);
+
 }
 
-//---------------------------------PART TWO-------------------------------------
 
-// let board = ["","","","","","","","","",];
+// let container.
+
+body.appendChild(container);
+
+//---------------------------------function player click------------------
+
+function clicked() {
+
+    if (playerTurn == "X") {
+
+        this.innerHTML = "X";
+        playerTurn = "O";
+
+    } else {
+
+        this.innerHTML = "O";
+        playerTurn = "X";
+
+    }
+
+}
 
 
-// var win1 = $("box1, box2, box3").length === 3
-// var win2 = $("box4, box5, box6").length === 3
-// var win3 = $("box9, box8, box9").length === 3
-// var win4 = $("box1, box4, box7").length === 3
-// var win5 = $("box2, box5, box8").length === 3
-// var win6 = $("box3, box6, box9").length === 3
-// var win7 = $("box1, box5, box9").length === 3
-// var win8 = $("box7, box5, box3").length === 3
+//---------------------------------get name of user------------------
+let namePara = document.createElement("p");
+
+body.appendChild(namePara);
+
+namePara.id = "name";
+
+function getName() {
+
+    let namepara = prompt("enter name");
+
+    if (name != null) {
+        document.getElementById("name").innerHTML = "Hello " + namepara;
+    } else {
+        "Hello friend";
+    }
+
+}
+
+let player = document.createElement('p');
+
+
+
+getName();
+
+//---------------------------------win statement------------------
+
+
+let win1 = get.getElementById("grid1, grid2, grid3").length === 3
+// let win2 = ("grid4, grid5, grid6").length === 3
+// let win3 = ("grid7, grid8, grid9").length === 3
+// let win4 = ("grid1, grid4, grid7").length === 3
+// let win5 = ("grid2, grid5, grid8").length === 3
+// let win6 = ("grid3, grid6, grid9").length === 3
+// let win7 = ("grid1, grid5, grid9").length === 3
+// let win8 = ("grid7, grid5, grid3").length === 3
