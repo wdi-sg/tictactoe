86c631f1e35c03387a16ba27191392694da3ea9b
Nur Amirah Sham <nuramirahsham@gmail.com>
2020-06-18 01:28:52 +0800

    added start button

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+//start button
+var start = document.querySelector("#start");
+var game = document.querySelector("#tictactoe");
+
+function hideButton() {
+  if (start.style.display = "block") {
+      start.style.display = "none";
+  } else {
+      start.style.display = "block";
+  }
+}
+
+function showBoard() {
+  if (game.style.display = "none") {
+      game.style.display = "block";
+  } else {
+      game.style.display = "none";
+  }
+}
+
+document.querySelector("#start").addEventListener('click', () => {
+    showBoard();
+    hideButton();
+});
 
 var board = {
   top: [null,"X",null],
