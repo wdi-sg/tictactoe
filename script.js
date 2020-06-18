c4500945c84635d7374abff4ebf3a6554fda4f8d
andicodetrf <andrea_lau92@hotmail.com>
2020-06-18 08:05:54 +0800

    finished version 2 but unable to fix span size from moving when there is a text within

--- script.js
+++ script.js
@@ -1,50 +1,85 @@
+//CREATE BOARD
+let body = document.querySelector("body");
+body.style.margin = "0px";
+body.style.padding = "0px";
+
+//CREATED DIV AND SPANS FOR GAMEBOARD 
+const gameboard =  document.createElement("div")
+gameboard.setAttribute("id", "gameboard")
+gameboard.style.border = "2px solid red"
+gameboard.style.width = "600px"
+gameboard.style.height = "470px"
+gameboard.style.textAlign = "center"
+gameboard.style.backgroundColor = "rgba(64,224,208,0.5)"
+
+document.body.appendChild(gameboard);
+
+for (let i = 1; i < 4; i++){
+    let createDiv = document.createElement("div")
+    createDiv.setAttribute("id", "row"+i);
+    createDiv.style.marginTop= "100px";
+    // createDiv.style.backgroundColor= "blue";
+    // createDiv.style.padding= "5px";
+    // gameboard.appendChild(createDiv)
+
+    for (let j = 1; j < 4; j++){
+        let createBox = document.createElement("span")
+        createBox.setAttribute("id","r" + i + "span" + j);
+        //createBox.innerHTML = "<span id = r" + i + "span" + j + " style = 'width:100px; height: 100px; background-color: grey; border: 2px solid black;'></span>"
+        // createBox.style.width = "50px"; //doesnt work its not a box
+        // createBox.style.height = "50px"; //ditto
+        createBox.style.backgroundColor = "lightgray";
+        createBox.style.border = "2px solid black";
+        createBox.style.padding = "50px 70px";
+        createBox.style.margin = "10px -1px";
+
+        createDiv.appendChild(createBox)
+    }
+
+    gameboard.appendChild(createDiv)
+
+}
+
+
+
 //-----GLOBAL VAR -----
-let clickCycle = 0; 
-let evenOrOdd;
+let state = 0; 
 
 const X = "X";
 const O = "O";
 
 
 //----DOM SELECT-----
-const mainGrid = document.querySelector("#mainGrid")
-const item = document.querySelector("#item")
+const gameboardSel = document.querySelector("#gameboard")
+const item = document.querySelector("span")
 
 
 
 //-----FUNCTIONS-------
-function checkEven(){
-    if (clickCycle%2 === 0){
-        evenOrOdd = "true"
-    } else {
-        evenOrOdd = "false"
-    }
-    return evenOrOdd;
-}
-
 
 function xTurn(itemTarget) {
     itemTarget.innerText = X;
-    clickCycle++;
+    itemTarget.style.fontSize = "20px";
+    state = 1;
 }
 
 function oTurn(itemTarget) {
     itemTarget.innerText = O;
-    clickCycle++;
+    itemTarget.style.fontSize = "20px";
+    state = 0;
 }
 
 
 // --------EVENT LISTENER-----------
 
-mainGrid.addEventListener('click', function(e){
+gameboardSel.addEventListener('click', function(e){
     let itemTarget = e.target;
     // itemTarget.innerText = "x";
-    checkEven();
-    switch(evenOrOdd){
-        case "true":
+    switch(state){
+        case 0:
             xTurn(itemTarget);
             break;
-        case "false": 
+        case 1: 
             oTurn(itemTarget);
             break;
         default:
